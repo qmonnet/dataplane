2ff657dc15a53ebddf55dcddfcc9c40a7c32009c
Fredi Raspall <fredi@githedgehog.com>
2025-02-04 22:51:29 +0100

    routing: add Adjacency type and Adjacency table
    Add minimal definitions for adjacency and adjacency table. We'll
    use the adjacency table to map directly connected next-hops to
    MAC addresses for the L2 dst MAC re-write. This table should play
    the role of an ARP/neigh cache, and we may populate it from the
    kernel neighbor system, or via netlink.
    
    Signed-off-by: Fredi Raspall <fredi@githedgehog.com>

---
+++ routing/src/adjacency.rs
@@ -0,0 +1,40 @@
+use crate::interface::IfIndex;
+use net::eth::mac::Mac;
+use std::collections::HashMap;
+use std::net::IpAddr;
+
+#[allow(dead_code)]
+pub struct Adjacency {
+    pub address: IpAddr,
+    pub mac: Mac,
+    pub ifindex: IfIndex,
+}
+pub struct AdjacencyTable(HashMap<IpAddr, Adjacency>);
+
+#[allow(dead_code)]
+impl AdjacencyTable {
+    pub(crate) fn new() -> Self {
+        Self(HashMap::new())
+        // Todo: use a fast hasher
+    }
+}
+
+#[allow(dead_code)]
+impl AdjacencyTable {
+    pub(crate) fn add_adjacency(&mut self, address: IpAddr, mac: Mac, ifindex: IfIndex) {
+        self.0.insert(
+            address,
+            Adjacency {
+                address,
+                mac,
+                ifindex,
+            },
+        );
+    }
+    pub(crate) fn del_adjacency(&mut self, address: &IpAddr) {
+        self.0.remove(address);
+    }
+    pub(crate) fn get_adjacency(&self, address: &IpAddr) -> Option<&Adjacency> {
+        self.0.get(address)
+    }
+}
--- routing/src/lib.rs
+++ routing/src/lib.rs
@@ -1,3 +1,4 @@
+mod adjacency;
 mod encapsulation;
 mod errors;
 mod interface;
