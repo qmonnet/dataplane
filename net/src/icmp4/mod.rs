6c07dae8a5c482f21f5d13ce08e2b2ae740c66c4
Daniel Noland <daniel@githedgehog.com>
2025-02-17 20:59:53 -0700

    ICMPv4 generation
    Signed-off-by: Daniel Noland <daniel@githedgehog.com>

--- net/src/icmp4/mod.rs
+++ net/src/icmp4/mod.rs
@@ -7,6 +7,10 @@ use crate::parse::{DeParse, DeParseError, LengthError, Parse, ParseError, ParseP
 use etherparse::Icmpv4Header;
 use std::num::NonZero;
 
+#[allow(unused_imports)] // re-export
+#[cfg(any(test, feature = "arbitrary"))]
+pub use contract::*;
+
 /// An `ICMPv4` header.
 #[derive(Debug, Clone, PartialEq, Eq)]
 pub struct Icmp4(Icmpv4Header);
@@ -59,5 +63,55 @@ impl ParsePayload for Icmp4 {
     /// We don't currently support parsing below the Icmp4 layer
     fn parse_payload(&self, _cursor: &mut Reader) -> Option<Self::Next> {
         None
+    }
+}
+
+#[cfg(any(test, feature = "arbitrary"))]
+mod contract {
+    use crate::icmp4::Icmp4;
+    use crate::parse::{Parse, ParseError};
+    use bolero::{Driver, TypeGenerator};
+
+    impl TypeGenerator for Icmp4 {
+        fn generate<D: Driver>(driver: &mut D) -> Option<Self> {
+            // TODO: 20 bytes is far too small to properly test the space of `Icmp4`
+            // We will need better error handling if we want to bump it up tho.
+            let buffer: [u8; 20] = driver.gen()?;
+            let icmp4 = match Icmp4::parse(&buffer) {
+                Ok((icmp4, _)) => icmp4,
+                Err(ParseError::Length(l)) => unreachable!("{:?}", l),
+                Err(ParseError::Invalid(e)) => unreachable!("{:?}", e),
+            };
+            Some(icmp4)
+        }
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use crate::icmp4::Icmp4;
+    use crate::parse::{DeParse, DeParseError, Parse, ParseError};
+
+    #[test]
+    fn parse_back() {
+        bolero::check!().with_type().for_each(|input: &Icmp4| {
+            // TODO: 20 bytes is far too small to properly test the space of `Icmp4`
+            // We will need better error handling if we want to bump it up tho.
+            let mut buffer = [0u8; 20];
+            let bytes_written = match input.deparse(&mut buffer) {
+                Ok(bytes_written) => bytes_written,
+                Err(DeParseError::Length(l)) => unreachable!("{:?}", l),
+                Err(DeParseError::Invalid(())) => {
+                    unreachable!()
+                }
+            };
+            let (parsed, bytes_read) = match Icmp4::parse(&buffer) {
+                Ok((parsed, bytes_read)) => (parsed, bytes_read),
+                Err(ParseError::Invalid(e)) => unreachable!("{e:?}"),
+                Err(ParseError::Length(l)) => unreachable!("{l:?}"),
+            };
+            assert_eq!(input, &parsed);
+            assert_eq!(bytes_written, bytes_read);
+        });
     }
 }
