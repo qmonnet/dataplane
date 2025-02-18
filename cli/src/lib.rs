f7322114a0c8f3a56396141e10efe773fb55acf6
Fredi Raspall <fredi@githedgehog.com>
2025-02-18 13:36:36 +0100

    feat(cli): add feature cli
    The feature is only needed by the cli binary. The only submodule
    from this crate needed by the dataplane is the cliproto.
    
    Signed-off-by: Fredi Raspall <fredi@githedgehog.com>

--- cli/src/lib.rs
+++ cli/src/lib.rs
@@ -1,9 +1,23 @@
+// SPDX-License-Identifier: Apache-2.0
+// Copyright Open Network Fabric Authors
+
 #[macro_use]
 extern crate enum_primitive;
 
+#[cfg(feature = "cli")]
 pub mod argsparse;
+
+/// CLI protocol
 pub mod cliproto;
+
+#[cfg(feature = "cli")]
 pub mod cmdtree;
+
+#[cfg(feature = "cli")]
 pub mod cmdtree_dp;
+
+#[cfg(feature = "cli")]
 pub mod completions;
+
+#[cfg(feature = "cli")]
 pub mod terminal;
