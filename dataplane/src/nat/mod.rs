697402a0883d71f12d490523b141a66b0ead2f1a
Quentin Monnet <qmo@qmon.net>
2025-02-28 11:44:53 +0000

    feat(nat): Add some missing methods to GlobalContext
    The global context objects used for NAT misses a few methods, in
    particular: a way to add a VPC to its table (VPCs were previously built
    exclusively from reading YAML files), a way to retrieve a particular VPC
    (we do it by VNI), and a way to retrieve a reference to an actual PIF
    once we have its name.
    
    Add these methods to the struct; they are meant to be used for NAT in a
    subsequent commit.
    
    Signed-off-by: Quentin Monnet <qmo@qmon.net>

--- dataplane/src/nat/mod.rs
+++ dataplane/src/nat/mod.rs
@@ -4,9 +4,10 @@
 mod fabric;
 mod prefixtrie;
 
-use crate::nat::fabric::Vpc;
+use crate::nat::fabric::{Pif, Vpc};
 use crate::nat::prefixtrie::PrefixTrie;
 
+use net::vxlan::Vni;
 use std::collections::HashMap;
 use std::fmt::Debug;
 use std::net::IpAddr;
@@ -28,7 +29,27 @@ impl GlobalContext {
     }
 
     #[tracing::instrument(level = "trace")]
+    fn insert_vpc(&mut self, vni: Vni, vpc: Vpc) {
+        vpc.iter_pifs().for_each(|pif| {
+            pif.iter_ips().for_each(|prefix| {
+                let _ = self.global_pif_trie.insert(prefix, pif.name().clone());
+            });
+        });
+        let _ = self.vpcs.insert(vni.as_u32(), vpc);
+    }
+
+    #[tracing::instrument(level = "trace")]
     fn find_pif_by_ip(&self, ip: &IpAddr) -> Option<String> {
         self.global_pif_trie.find_ip(ip)
+    }
+
+    #[tracing::instrument(level = "trace")]
+    fn get_vpc(&self, vni: Vni) -> Option<&Vpc> {
+        self.vpcs.get(&vni.as_u32())
+    }
+
+    #[tracing::instrument(level = "trace")]
+    fn find_pif_by_name(&self, name: &String) -> Option<&Pif> {
+        self.vpcs.values().find_map(|vpc| vpc.get_pif(name))
     }
 }
