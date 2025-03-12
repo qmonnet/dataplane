bf0d1f5946ebac807b2ae21b6522093f0b609ac0
Quentin Monnet <qmo@qmon.net>
2025-03-12 16:15:51 +0000

    feat(nat): Extend IpList to account for excluded prefixes
    Initially, the IpList struct represented a flattened set of prefixes. In
    the previous commit, we completely restructured the submodule to make
    the object represent a single prefix instead. This commit brings
    additional changes.
    
    This commit is based on the current version of the proposed API for the
    Gateway [0]. In particular, let's consider the following example from
    the document:
    
        apiVersion: gateway.githedgehog.com/v1alpha1
        kind: Peering
        metadata:
          name: vpc-1--vpc-2
        spec:
          peering:
            vpc-1:
              ips:
                - cidr: 10.1.1.0/24
                - not: 10.1.1.42/32 # that's just a syntactic sugar to avoid enumerating all the subnets explicitly
              as:
                - 192.168.1.0/24
                - not: 192.168.1.7/32 # that's just a syntactic sugar to avoid enumerating all the subnets explicitly
            vpc-2:
              ips:
                - cidr: 10.1.1.0/24
              as:
                - 192.168.2.0/24
    
    We note that the list of endpoint prefixes ("ips") and corresponding
    address sets for NAT ("as") can be expressed as prefixes ("cidrs") to
    which we can "exclusion prefixes" ("not").
    
    This commit is a first attempt at supporting this semantics in the nat
    module, and in particular in the IpList submodule, by accounting for
    exclusion prefixes when retrieving addresses at a given offset, or the
    offset of a given address from an IpList. Note that the code is not
    exercised yet, because we don't have a configuration step that accounts
    for these exclusion prefixes, but we'll provide unit tests in a
    follow-up commit.
    
    [0] https://github.com/githedgehog/gateway/blob/70010a597b325fb3a3948c73dce44c5865fc973a/docs/proposed-api.md
    
    Signed-off-by: Quentin Monnet <qmo@qmon.net>

--- dataplane/src/nat/iplist.rs
+++ dataplane/src/nat/iplist.rs
@@ -1,22 +1,108 @@
 // SPDX-License-Identifier: Apache-2.0
 // Copyright Open Network Fabric Authors
 
-use iptrie::IpPrefix;
+//! A data structure interface presenting a list of IP addresses within a main
+//! prefix, accounting for optional exclusion prefixes within this range.
+
 use routing::prefix::Prefix;
 use std::fmt::Debug;
 use std::net::{IpAddr, Ipv4Addr, Ipv6Addr};
 
+#[tracing::instrument(level = "trace")]
+fn addr_higher_than_prefix_start(ip: &IpAddr, prefix: &Prefix) -> bool {
+    match (ip, prefix.as_address()) {
+        (IpAddr::V4(ip), IpAddr::V4(start)) => ip.to_bits() >= start.to_bits(),
+        (IpAddr::V6(ip), IpAddr::V6(start)) => ip.to_bits() >= start.to_bits(),
+        _ => panic!("Cannot compare address and prefix of different IP versions"),
+    }
+}
+
+#[tracing::instrument(level = "trace")]
+fn addr_offset_in_prefix(ip: &IpAddr, prefix: &Prefix) -> Option<u128> {
+    if !prefix.covers_addr(ip) {
+        return None;
+    }
+    match (ip, prefix.as_address()) {
+        (IpAddr::V4(ip), IpAddr::V4(start)) => {
+            Some(u128::from(ip.to_bits()) - u128::from(start.to_bits()))
+        }
+        (IpAddr::V6(ip), IpAddr::V6(start)) => Some(ip.to_bits() - start.to_bits()),
+        // We can't have the prefix covering the address if we have an IP
+        // version mismatch, and we'd have returned from the function earlier.
+        _ => unreachable!(),
+    }
+}
+
+#[tracing::instrument(level = "trace")]
+fn addr_from_prefix_offset(prefix: &Prefix, offset: u128) -> Option<IpAddr> {
+    if offset >= prefix.size() {
+        return None;
+    }
+    match prefix.as_address() {
+        IpAddr::V4(start) => {
+            let bits = start.to_bits() + u32::try_from(offset).ok()?;
+            Some(IpAddr::V4(Ipv4Addr::from(bits)))
+        }
+        IpAddr::V6(start) => {
+            let bits = start.to_bits() + offset;
+            Some(IpAddr::V6(Ipv6Addr::from(bits)))
+        }
+    }
+}
+
+/// Error type for [`IpList`] operations.
+#[derive(thiserror::Error, Debug, PartialEq, Eq)]
+pub enum IpListError {
+    #[error("IP version mismatch")]
+    IpVersionMismatch,
+    #[error("Exclusion prefix is not within the main prefix range")]
+    ExcludePrefixOutOfRange,
+    #[error("No addresses left after excluding prefixes")]
+    NoAddressesLeft,
+}
+
+/// Represents a list of IP addresses within a given prefix range, accounting
+/// for exclusion prefixes within this range.
 #[derive(Debug, Clone)]
 pub struct IpList {
     prefix: Prefix,
+    // Sorted by start address; no overlap allowed
+    excludes: Vec<Prefix>,
 }
 
 impl IpList {
+    /// Creates a new [`IpList`] with the given prefix and optional exclusion prefixes.
     #[tracing::instrument(level = "trace")]
-    fn new(prefix: Prefix) -> Self {
-        IpList { prefix }
+    fn new(prefix: Prefix, excludes_opt: Option<Vec<Prefix>>) -> Self {
+        let mut list = IpList {
+            prefix,
+            excludes: vec![],
+        };
+        if let Some(excludes) = excludes_opt {
+            for exclude in excludes {
+                // TODO: Handle errors properly
+                list.add_exclude(exclude).ok();
+            }
+        }
+        list
     }
 
+    /// Generates a pair of [`IpList`] objects representing the current prefix
+    /// for a given IP address, and the corresponding target prefix for NAT
+    /// translation.
+    ///
+    /// For a given `current_ip`, `current_prefixes` is typically an iterator
+    /// over the list of prefixes in the PIF that the IP address belongs to;
+    /// `target_prefixes` is typically an iterator over the list of prefixes in
+    /// the NAT configuration that we may translate the IP address to. The
+    /// function returns a pair of [`IpList`] objects, one representing the
+    /// specific set of addresses that `current_ip` belongs to (subset of
+    /// `current_prefixes`), the other one being the corresponding set of target
+    /// addresses, of the same size, such that a 1:1 mapping can be established
+    /// between the two sets for NAT translation.
+    ///
+    /// Arguments `current_prefixes` and `target_prefixes` do not contain
+    /// exclusion prefixes; this may be subject to change in the future.
     pub fn generate_ranges<'a, I, J>(
         current_prefixes: I,
         target_prefixes: J,
@@ -42,49 +128,115 @@ impl IpList {
             }
             if prefix_from_current.covers_addr(current_ip) {
                 return Some((
-                    IpList::new(prefix_from_current.clone()),
-                    IpList::new(prefix_from_target.clone()),
+                    IpList::new(prefix_from_current.clone(), None),
+                    IpList::new(prefix_from_target.clone(), None),
                 ));
             }
         }
         None
     }
 
+    /// Adds an exclusion prefix to the [`IpList`].
+    #[tracing::instrument(level = "trace")]
+    pub fn add_exclude(&mut self, prefix: Prefix) -> Result<(), IpListError> {
+        // Ensure we have no IP version mismatch
+        match (&self.prefix, &prefix) {
+            (&Prefix::IPV4(_), &Prefix::IPV4(_)) | (&Prefix::IPV6(_), &Prefix::IPV6(_)) => (),
+            _ => return Err(IpListError::IpVersionMismatch),
+        }
+
+        if !self.prefix.covers(&prefix) {
+            return Err(IpListError::ExcludePrefixOutOfRange);
+        }
+
+        // Skip if the prefix is already in list
+        let mut excludes_size = 0;
+        for exclude in &self.excludes {
+            if exclude.covers(&prefix) {
+                return Ok(());
+            }
+            // Count total excluded addresses, not counting overlaps
+            if !prefix.covers(exclude) {
+                excludes_size += exclude.size();
+            }
+        }
+
+        // Forbid excluding all the addresses from the main prefix
+        if excludes_size + prefix.size() == self.prefix.size() {
+            return Err(IpListError::NoAddressesLeft);
+        }
+
+        // Discard any existing exclude prefixes covered by the new prefix
+        self.excludes.retain(|e| !prefix.covers(e));
+
+        // Insert the prefix while preserving the order, based on start address
+        let prefix_start = match prefix.as_address() {
+            IpAddr::V4(start) => u128::from(start.to_bits()),
+            IpAddr::V6(start) => start.to_bits(),
+        };
+        let idx = self
+            .excludes
+            .binary_search_by_key(&prefix_start, |exclude| match exclude.as_address() {
+                IpAddr::V4(start) => u128::from(start.to_bits()),
+                IpAddr::V6(start) => start.to_bits(),
+            })
+            .unwrap_or_else(|e| e);
+        self.excludes.insert(idx, prefix);
+        Ok(())
+    }
+
+    /// Gets the number of addresses covered by the [`IpList`].
+    /// This is the number of addresses covered by the main prefix, minus the
+    /// number of addresses covered by the exclusion prefixes.
+    #[tracing::instrument(level = "trace")]
+    pub fn size(&self) -> u128 {
+        self.excludes
+            .iter()
+            .fold(self.prefix.size(), |acc, exclude| acc - exclude.size())
+    }
+
+    /// Checks if the [`IpList`] covers the given [`IpAddr`]. Returns `true` if
+    /// the [`IpList`] covers the given [`IpAddr`], `false` otherwise.
+    #[tracing::instrument(level = "trace")]
+    pub fn covers_addr(&self, ip: &IpAddr) -> bool {
+        self.prefix.covers_addr(ip) && !self.excludes.iter().any(|exclude| exclude.covers_addr(ip))
+    }
+
     /// Returns the offset of the given [`IpAddr`] in the [`IpList`].
     #[tracing::instrument(level = "trace")]
     pub fn get_offset(&self, ip: &IpAddr) -> Option<u128> {
-        if !self.prefix.covers_addr(ip) {
+        if !self.covers_addr(ip) {
             return None;
         }
-        match (ip, self.prefix.as_address()) {
-            (IpAddr::V4(ip), IpAddr::V4(start)) => {
-                Some(u128::from(ip.to_bits()) - u128::from(start.to_bits()))
+
+        let mut offset_in_prefix = addr_offset_in_prefix(ip, &self.prefix)?;
+        for exclude in &self.excludes {
+            if addr_higher_than_prefix_start(ip, exclude) {
+                offset_in_prefix -= exclude.size();
+            } else {
+                break;
             }
-            (IpAddr::V6(ip), IpAddr::V6(start)) => Some(ip.to_bits() - start.to_bits()),
-            // The IP address cannot be covered by the prefix if there is a
-            // mismatch betwen the IP versions, so we'd have returned earlier in
-            // the function.
-            _ => unreachable!(),
         }
+        Some(offset_in_prefix)
     }
 
     /// Returns the IP address at the given offset within the [`IpList`].
     #[tracing::instrument(level = "trace")]
-    pub fn get_addr(&self, offset: u128) -> Option<IpAddr> {
-        if offset >= self.prefix.size() {
+    pub fn get_addr(&self, offset_in_list: u128) -> Option<IpAddr> {
+        if offset_in_list >= self.size() {
             return None;
         }
-        match self.prefix {
-            Prefix::IPV4(p) => {
-                let start = p.network().to_bits();
-                let bits = start + u32::try_from(offset).ok()?;
-                Some(IpAddr::V4(Ipv4Addr::from_bits(bits)))
-            }
-            Prefix::IPV6(p) => {
-                let start = p.network().to_bits();
-                let bits = start + offset;
-                Some(IpAddr::V6(Ipv6Addr::from_bits(bits)))
+
+        let mut offset_in_prefix = offset_in_list;
+        let mut addr = addr_from_prefix_offset(&self.prefix, offset_in_prefix)?;
+        for exclude in &self.excludes {
+            if addr_higher_than_prefix_start(&addr, exclude) {
+                offset_in_prefix += exclude.size();
+                addr = addr_from_prefix_offset(&self.prefix, offset_in_prefix)?;
+            } else {
+                break;
             }
         }
+        Some(addr)
     }
 }
