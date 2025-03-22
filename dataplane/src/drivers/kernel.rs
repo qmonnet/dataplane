98830c675f2a4f7049a60ebe5c498cfa3916de33
Fredi Raspall <fredi@githedgehog.com>
2025-03-22 16:34:53 +0100

    feat(dataplnane): scaffold DriverKernel object
    The kernel driver requires TestBuffer feature from net.
    
    Signed-off-by: Fredi Raspall <fredi@githedgehog.com>

--- dataplane/src/drivers/kernel.rs
+++ dataplane/src/drivers/kernel.rs
@@ -2,3 +2,20 @@
 // Copyright Open Network Fabric Authors
 
 //! Kernel dataplane driver
+
+use crate::CmdArgs;
+use net::buffer::test_buffer::TestBuffer;
+use pipeline::{self, DynPipeline, NetworkFunction};
+use tracing::debug;
+
+pub struct DriverKernel;
+
+impl DriverKernel {
+    pub fn start(
+        args: impl IntoIterator<Item = impl AsRef<str>>,
+        pipeline: DynPipeline<TestBuffer>,
+    ) {
+        debug!("Entering Kernel worker IO loop");
+        loop { /* TODO */ }
+    }
+}
