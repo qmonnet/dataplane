3044de43c013da8f6977f2cd4c6363f451dda20f
Daniel Noland <daniel@githedgehog.com>
2025-03-26 20:07:09 -0600

    feat: intro interface name
    This is a checked string to make sure that we obey Linux interface
    naming rules
    
    Signed-off-by: Daniel Noland <daniel@githedgehog.com>

--- net/src/interface/mod.rs
+++ net/src/interface/mod.rs
@@ -53,3 +53,98 @@ impl From<InterfaceIndex> for u32 {
         value.to_u32()
     }
 }
+
+const MAX_LEN: usize = 16;
+
+/// A string which has been checked to be a legal linux network interface name.
+///
+/// Legal network interface names are composed only of alphanumeric ASCII characters, `.`, `-`, and
+/// `_` and which are terminated with a null (`\0`) character.
+///
+/// The maximum legal length of an `InterfaceName` is 16 bytes (including the terminating null).
+/// Thus, the _effective_ maximum length is 15 bytes (not characters).
+#[repr(transparent)]
+#[derive(serde::Serialize, serde::Deserialize)]
+#[serde(try_from = "String", into = "String")]
+#[derive(Clone, Ord, PartialOrd, Eq, PartialEq, Hash, Debug)]
+pub struct InterfaceName(String);
+
+impl Display for InterfaceName {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}", self.0)
+    }
+}
+
+impl InterfaceName {
+    /// The maximum legal length of a linux network interface name (including the trailing NUL)
+    pub const MAX_LEN: usize = MAX_LEN;
+}
+
+/// Errors which may occur when mapping a general `String` into an `InterfaceName`.
+#[derive(Debug, Clone, PartialEq, Eq, PartialOrd, Ord, Hash, thiserror::Error)]
+pub enum IllegalInterfaceName {
+    /// A string which is longer than 15 characters was submitted.
+    #[error("interface name must be at least one character")]
+    Empty,
+    /// A string which is longer than 15 characters was submitted.
+    #[error("interface name {0} is too long")]
+    TooLong(String),
+    /// The string must not contain an interior null character.
+    #[error("interface name {0} contains interior null characters")]
+    InteriorNull(String),
+    /// The supplied string is not legal ASCII.
+    #[error("interface name {0} is not ascii")]
+    NotAscii(String),
+    /// The supplied string contains an illegal character.
+    #[error(
+        "interface name {0} contains illegal characters (only alphanumeric ASCII and .-_ are permitted)"
+    )]
+    IllegalCharacters(String),
+}
+
+impl TryFrom<String> for InterfaceName {
+    type Error = IllegalInterfaceName;
+
+    fn try_from(value: String) -> Result<Self, Self::Error> {
+        const LEGAL_PUNCT: [char; 3] = ['.', '-', '_'];
+        if value.is_empty() {
+            return Err(IllegalInterfaceName::Empty);
+        }
+        if value.contains('\0') {
+            return Err(IllegalInterfaceName::InteriorNull(value));
+        }
+        if !value.is_ascii() {
+            return Err(IllegalInterfaceName::NotAscii(value));
+        }
+        if !value
+            .chars()
+            .all(|c| c.is_alphanumeric() || LEGAL_PUNCT.contains(&c))
+        {
+            return Err(IllegalInterfaceName::IllegalCharacters(value));
+        }
+        if value.len() > InterfaceName::MAX_LEN {
+            return Err(IllegalInterfaceName::TooLong(value));
+        }
+        Ok(InterfaceName(value))
+    }
+}
+
+impl TryFrom<&str> for InterfaceName {
+    type Error = IllegalInterfaceName;
+
+    fn try_from(value: &str) -> Result<Self, Self::Error> {
+        Self::try_from(value.to_string())
+    }
+}
+
+impl From<InterfaceName> for String {
+    fn from(value: InterfaceName) -> Self {
+        value.0.as_str().to_string()
+    }
+}
+
+impl AsRef<str> for InterfaceName {
+    fn as_ref(&self) -> &str {
+        self.0.as_str()
+    }
+}
