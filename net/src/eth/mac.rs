1e3c9b20e8ece7c3cedce1a6dc2b4e9a8be91423
Fredi Raspall <fredi@githedgehog.com>
2025-03-28 11:54:23 +0100

    feat(net): Display MAC hex non-capital
    Signed-off-by: Fredi Raspall <fredi@githedgehog.com>

--- net/src/eth/mac.rs
+++ net/src/eth/mac.rs
@@ -136,7 +136,7 @@ impl Display for Mac {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         write!(
             f,
-            "{:<02X}:{:<02X}:{:<02X}:{:<02X}:{:<02X}:{:<02X}",
+            "{:<02x}:{:<02x}:{:<02x}:{:<02x}:{:<02x}:{:<02x}",
             self.0[0], self.0[1], self.0[2], self.0[3], self.0[4], self.0[5]
         )
     }
