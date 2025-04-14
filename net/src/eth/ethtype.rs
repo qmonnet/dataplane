19d6c0f7d8b235fe8c0885a0d9156ef55fae296e
Quentin Monnet <qmo@qmon.net>
2025-04-14 11:34:38 +0100

    fix(net/ethtype): Make serde attributes dependent on "serde" feature
    The use of dependency crate "serde" in the "net" package is optional,
    based on the "serde" feature for the package. As a consequence, we
    cannot always derive the serde traits without taking precautions inside
    of the package.
    
    This issue has crept in in spite of the CI checks, because CI does not
    build every package independently. We observed the build failure with:
    
        $ just cargo build -p net
    
    We then found the relevant commit that introduced the issue by
    bissecting, and observed we could fix the issue, on top of that commit,
    by enabling "serde" by running instead:
    
        $ just cargo build -p net -F serde
    
    However, the issue doesn't appear at all by running only:
    
        $ just cargo build
    
    This is likely because when building from the root of the repository, we
    must address requirements from all packages, including the "dataplane"
    package that requires the "net" package to be built with its "serde"
    feature, thus masking the issue.
    
    Anyway, fix the bug by making traits derivation conditional to the use
    of the "serde" feature in ethtype.rs.
    
    Fixes: 55d2b2fc46b5 (feat: convenience methods for ethtype)
    Reported-by: Fredi Raspall <fredi@githedgehog.com>
    Signed-off-by: Quentin Monnet <qmo@qmon.net>

--- net/src/eth/ethtype.rs
+++ net/src/eth/ethtype.rs
@@ -17,10 +17,9 @@ use etherparse::EtherType;
 /// 2. Permit the implementation of the `TypeGenerator` trait on this type
 ///    to allow us to property test the rest of our code.
 #[repr(transparent)]
-#[derive(
-    Debug, Copy, Clone, PartialEq, Eq, PartialOrd, Ord, Hash, serde::Serialize, serde::Deserialize,
-)]
-#[serde(from = "u16", into = "u16")]
+#[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
+#[cfg_attr(feature = "serde", serde(from = "u16", into = "u16"))]
+#[derive(Debug, Copy, Clone, PartialEq, Eq, PartialOrd, Ord, Hash)]
 pub struct EthType(pub(crate) EtherType);
 
 impl EthType {
