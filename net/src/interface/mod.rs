208e3be507167205dbd558e1bcd5059cfb3f33d9
Daniel Noland <daniel@githedgehog.com>
2025-04-14 19:56:20 -0600

    feat(net): impl TypeGenerator for various interface types
    Signed-off-by: Daniel Noland <daniel@githedgehog.com>

--- net/src/interface/mod.rs
+++ net/src/interface/mod.rs
@@ -424,3 +424,93 @@ impl TryFrom<LinkMessage> for Interface {
         builder.build()
     }
 }
+
+#[cfg(any(test, feature = "arbitrary"))]
+mod contract {
+    use crate::interface::{
+        AdminState, Interface, InterfaceIndex, InterfaceName, InterfaceProperties, OperationalState,
+    };
+    use bolero::{Driver, TypeGenerator};
+
+    impl TypeGenerator for InterfaceIndex {
+        fn generate<D: Driver>(driver: &mut D) -> Option<Self> {
+            Some(Self(driver.produce()?))
+        }
+    }
+
+    impl TypeGenerator for AdminState {
+        fn generate<D: Driver>(driver: &mut D) -> Option<Self> {
+            match driver.produce::<u8>()? {
+                x if x % 2 == 0 => Some(AdminState::Down),
+                _ => Some(AdminState::Up),
+            }
+        }
+    }
+
+    impl TypeGenerator for OperationalState {
+        fn generate<D: Driver>(driver: &mut D) -> Option<Self> {
+            match driver.produce::<u8>()? {
+                x if x % 4 == 0 => Some(OperationalState::Up),
+                x if x % 4 == 1 => Some(OperationalState::Down),
+                x if x % 4 == 2 => Some(OperationalState::Unknown),
+                x if x % 4 == 3 => Some(OperationalState::Complex),
+                _ => unreachable!(),
+            }
+        }
+    }
+
+    pub struct LegalInterfaceName;
+
+    impl TypeGenerator for InterfaceName {
+        fn generate<D: Driver>(driver: &mut D) -> Option<Self> {
+            const NUM_LEGAL_CHARS: u8 = 65;
+            const LEGAL_CHARS: [char; NUM_LEGAL_CHARS as usize] = [
+                'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p',
+                'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F',
+                'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V',
+                'W', 'X', 'Y', 'Z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '.', '_',
+                '-',
+            ];
+            #[allow(clippy::cast_possible_truncation)] // const eval
+            let target_length =
+                (1 + (driver.produce::<u8>()? % (InterfaceName::MAX_LEN as u8 - 2))) as usize;
+            let mut base_string = String::with_capacity(target_length + 1);
+            for _ in 0..target_length {
+                let selected_char_index = (driver.produce::<u8>()? % NUM_LEGAL_CHARS) as usize;
+                let selected_char = LEGAL_CHARS[selected_char_index];
+                base_string.push(selected_char);
+            }
+            if base_string == "." || base_string == ".." {
+                base_string.push('_');
+            }
+            #[allow(clippy::unwrap_used)] // safe by contract
+            Some(InterfaceName::try_from(base_string.as_str()).unwrap())
+        }
+    }
+
+    impl TypeGenerator for InterfaceProperties {
+        fn generate<D: Driver>(driver: &mut D) -> Option<Self> {
+            match driver.produce::<u8>()? {
+                x if x % 4 == 0 => Some(InterfaceProperties::Bridge(driver.produce()?)),
+                x if x % 4 == 1 => Some(InterfaceProperties::Vtep(driver.produce()?)),
+                x if x % 4 == 2 => Some(InterfaceProperties::Vrf(driver.produce()?)),
+                x if x % 4 == 3 => Some(InterfaceProperties::Other),
+                _ => unreachable!(),
+            }
+        }
+    }
+
+    impl TypeGenerator for Interface {
+        fn generate<D: Driver>(driver: &mut D) -> Option<Self> {
+            Some(Self {
+                admin_state: driver.produce()?,
+                controller: driver.produce()?,
+                index: driver.produce()?,
+                mac: driver.produce()?,
+                name: driver.produce()?,
+                operational_state: driver.produce()?,
+                properties: driver.produce()?,
+            })
+        }
+    }
+}
