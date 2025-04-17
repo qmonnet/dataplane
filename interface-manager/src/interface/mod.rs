48f73d6f249f3bf855987e66580a06bce68ac191
Daniel Noland <daniel@githedgehog.com>
2025-04-16 21:05:54 -0600

    feat(interface-manager): InterfacePropertiesSpec
    This is the spec for interface type specific properties.
    
    Signed-off-by: Daniel Noland <daniel@githedgehog.com>

--- interface-manager/src/interface/mod.rs
+++ interface-manager/src/interface/mod.rs
@@ -5,12 +5,70 @@
 
 mod association;
 mod bridge;
+mod properties;
 mod vrf;
 mod vtep;
 
 #[allow(unused_imports)] // re-export
 pub use association::*;
 #[allow(unused_imports)] // re-export
+pub use bridge::*;
+#[allow(unused_imports)] // re-export
 pub use vrf::*;
 #[allow(unused_imports)] // re-export
 pub use vtep::*;
+
+use crate::interface::properties::InterfacePropertiesSpec;
+use derive_builder::Builder;
+use multi_index_map::MultiIndexMap;
+use net::eth::mac::SourceMac;
+use net::interface::{AdminState, InterfaceIndex, InterfaceName};
+use serde::{Deserialize, Serialize};
+
+/// The specified / intended state for a network interface.
+///
+/// This type represents a "plan" in that it consists of goals to be realized, not observed external
+/// state.
+#[derive(
+    Builder,
+    Clone,
+    Debug,
+    Deserialize,
+    Eq,
+    Hash,
+    MultiIndexMap,
+    Ord,
+    PartialEq,
+    PartialOrd,
+    Serialize,
+)]
+#[cfg_attr(any(test, feature = "bolero"), derive(bolero::TypeGenerator))]
+#[multi_index_derive(Debug, Clone, Default, Serialize, Deserialize)]
+pub struct InterfaceSpec {
+    /// The intended name of the network interface.
+    #[multi_index(hashed_unique)]
+    pub name: InterfaceName,
+    /// The MAC address to be assigned to the interface.  If set to `None `, then the operating
+    /// system will pick for you.
+    #[builder(default)]
+    pub mac: Option<SourceMac>,
+    /// The intended administrative state of the network interface.
+    ///
+    /// Note that it is never possible to specify the operational state of a network interface.
+    /// At most, you may articulate if a network interface _should_ be up or down.
+    pub admin_state: AdminState,
+    /// If this network interface is supposed to be a member / "slave" / controlled by another
+    /// network interface, then specify that other interface's index here.
+    ///
+    /// Note that the end user rarely needs to set this directly (principally because this
+    /// information is rarely available directly).
+    /// Instead, the reconciliation algorithm will fill in this information as it becomes
+    ///  available.
+    ///
+    /// Note that the default (`None`) indicates that this interface _should not_ be controlled by
+    /// / associated with any other network interface.
+    #[builder(default)]
+    pub controller: Option<InterfaceIndex>,
+    /// Interface-specific properties.
+    pub properties: InterfacePropertiesSpec,
+}
---
+++ interface-manager/src/interface/properties.rs
@@ -0,0 +1,88 @@
+// SPDX-License-Identifier: Apache-2.0
+// Copyright Open Network Fabric Authors
+
+use crate::interface::{BridgePropertiesSpec, VrfPropertiesSpec, VtepPropertiesSpec};
+use net::interface::InterfaceProperties;
+use rekon::AsRequirement;
+use serde::{Deserialize, Serialize};
+
+/// The planned properties of a network interface.
+#[derive(Clone, Debug, Eq, Hash, Ord, PartialEq, PartialOrd, Deserialize, Serialize)]
+#[cfg_attr(any(test, feature = "bolero"), derive(bolero::TypeGenerator))]
+pub enum InterfacePropertiesSpec {
+    /// The planned properties of a bridge.
+    Bridge(BridgePropertiesSpec),
+    /// The planned properties of a vtep (vxlan device).
+    Vtep(VtepPropertiesSpec),
+    /// The planned properties of a vrf
+    Vrf(VrfPropertiesSpec),
+}
+
+impl AsRequirement<InterfacePropertiesSpec> for InterfaceProperties {
+    type Requirement<'a>
+        = Option<InterfacePropertiesSpec>
+    where
+        Self: 'a;
+
+    fn as_requirement<'a>(&self) -> Self::Requirement<'a> {
+        Some(match self {
+            InterfaceProperties::Bridge(props) => {
+                InterfacePropertiesSpec::Bridge(props.as_requirement())
+            }
+            InterfaceProperties::Vtep(props) => {
+                InterfacePropertiesSpec::Vtep(props.as_requirement()?)
+            }
+            InterfaceProperties::Vrf(props) => InterfacePropertiesSpec::Vrf(props.as_requirement()),
+            InterfaceProperties::Other => return None,
+        })
+    }
+}
+
+impl PartialEq<InterfaceProperties> for InterfacePropertiesSpec {
+    fn eq(&self, other: &InterfaceProperties) -> bool {
+        match other.as_requirement() {
+            None => false,
+            Some(other) => other == *self,
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::InterfacePropertiesSpec;
+    use net::interface::InterfaceProperties;
+    use rekon::AsRequirement;
+
+    #[test]
+    fn as_requirement_obeys_contract() {
+        bolero::check!()
+            .with_type()
+            .for_each(
+                |observed: &InterfaceProperties| match observed.as_requirement() {
+                    None => {}
+                    Some(observed_req) => {
+                        assert_eq!(&observed_req, observed);
+                        assert_eq!(observed_req, observed.as_requirement().unwrap());
+                    }
+                },
+            );
+    }
+
+    #[test]
+    fn equality_meaning() {
+        bolero::check!().with_type().for_each(
+            |(requirement, observation): &(InterfacePropertiesSpec, InterfaceProperties)| {
+                if requirement == observation {
+                    assert_eq!(requirement, &observation.as_requirement().unwrap());
+                } else {
+                    match observation.as_requirement() {
+                        None => {}
+                        Some(observation_req) => {
+                            assert_ne!(requirement, &observation_req);
+                        }
+                    }
+                }
+            },
+        );
+    }
+}
