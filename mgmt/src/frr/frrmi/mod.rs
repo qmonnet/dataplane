f84ec7662193a0ce78d81d39cc2ec969350e8326
Fredi Raspall <fredi@githedgehog.com>
2025-04-29 23:16:57 +0200

    feat(mgmt): let frrmi be async
    Signed-off-by: Fredi Raspall <fredi@githedgehog.com>

--- mgmt/src/frr/frrmi/mod.rs
+++ mgmt/src/frr/frrmi/mod.rs
@@ -11,10 +11,12 @@ use crate::models::external::configdb::gwconfig::GenId;
 use std::fs;
 use std::net::Shutdown;
 use std::os::unix::fs::PermissionsExt;
-use std::os::unix::net::UnixDatagram;
+//use std::os::unix::net::UnixDatagram;
 use std::path::Path;
 use thiserror::Error;
 use tracing::{debug, error, info};
+
+use tokio::net::UnixDatagram;
 
 #[derive(Error, Debug)]
 pub enum FrrErr {
@@ -34,9 +36,8 @@ pub enum FrrErr {
     ReloadErr(String),
 }
 
-fn open_unix_sock<P: AsRef<Path> + ?Sized + std::fmt::Display>(
+fn open_unix_sock_async<P: AsRef<Path> + ?Sized + std::fmt::Display>(
     bind_addr: &P,
-    non_blocking: bool,
 ) -> Result<UnixDatagram, &'static str> {
     let _ = std::fs::remove_file(bind_addr);
     let sock = UnixDatagram::bind(bind_addr).map_err(|_| "Failed to bind socket")?;
@@ -45,8 +46,6 @@ fn open_unix_sock<P: AsRef<Path> + ?Sized + std::fmt::Display>(
         .permissions();
     perms.set_mode(0o777); // fixme: make this more restrictive
     fs::set_permissions(bind_addr, perms).map_err(|_| "Failure setting permissions")?;
-    sock.set_nonblocking(non_blocking)
-        .map_err(|_| "Failed to set non-blocking")?;
     debug!("Created unix sock and bound to {bind_addr}");
     Ok(sock)
 }
@@ -57,7 +56,7 @@ pub struct FrrMi {
 impl FrrMi {
     pub fn new(bind_addr: &str, remote_addr: &str) -> Result<FrrMi, FrrErr> {
         // fixme: use non-blocking
-        let sock = open_unix_sock(bind_addr, false).map_err(FrrErr::FailOpen)?;
+        let sock = open_unix_sock_async(bind_addr).map_err(FrrErr::FailOpen)?;
         sock.connect(remote_addr).map_err(|e| {
             error!("Failed to connect to {remote_addr}");
             FrrErr::FailConnect(e.to_string())
@@ -69,19 +68,19 @@ impl FrrMi {
         info!("Shutting down frrmi...");
         let _ = self.sock.shutdown(Shutdown::Both);
     }
-    pub fn apply_config(&self, genid: GenId, config: &ConfigBuilder) -> Result<(), FrrErr> {
+    pub async fn apply_config(&self, genid: GenId, config: &ConfigBuilder) -> Result<(), FrrErr> {
         info!("Applying config {genid} over frrmi...");
         let conf_str = config.to_string();
         let length = conf_str.len() as u64;
 
         /* send config length */
-        self.sock.send(&length.to_ne_bytes()).map_err(|e| {
+        self.sock.send(&length.to_ne_bytes()).await.map_err(|e| {
             error!("Fatal: Failed to send msg length: {}", e.kind());
             FrrErr::FailSend(e.to_string())
         })?;
 
         /* send config */
-        self.sock.send(conf_str.as_bytes()).map_err(|e| {
+        self.sock.send(conf_str.as_bytes()).await.map_err(|e| {
             error!("Fatal: Failed to send config for gen {genid}: {}", e.kind());
             FrrErr::FailSend(e.to_string())
         })?;
@@ -92,6 +91,7 @@ impl FrrMi {
         let mut rx_buff = vec![0u8; 1024];
         self.sock
             .recv(&mut rx_buff)
+            .await
             .map_err(|e| FrrErr::ErrorRx(e.to_string()))?;
 
         info!("Successfully applied config with id {genid}");
@@ -107,9 +107,9 @@ mod tests {
     use crate::processor::tests::test::sample_external_config;
     use tracing_test::traced_test;
 
-    #[test]
+    #[tokio::test]
     #[traced_test]
-    fn test_reloader() {
+    async fn test_reloader() {
         /* build some sample config */
         let external = sample_external_config();
         let mut config = GwConfig::new(external);
@@ -117,16 +117,25 @@ mod tests {
         config.build_internal_config().expect("Should succeed");
         let rendered = config.internal.as_ref().unwrap().render(&config);
 
+        /* create faked frr-agent */
+        let sock = open_unix_sock_async("/tmp/frr-agent.sock").expect("Should succeed");
+        tokio::spawn(async move {
+            let mut rx_buff = vec![0u8; 8192];
+            let (_, _) = sock.recv_from(&mut rx_buff).await.unwrap();
+            let (_, _) = sock.recv_from(&mut rx_buff).await.unwrap();
+            sock.send_to("Ok".to_string().as_bytes(), "/tmp/frrmi.sock")
+                .await
+                .unwrap();
+        });
+
         /* open frrmi */
-        let frrmi = FrrMi::new("/var/run/frr/frrmi.sock", "/var/run/frr/frr-agent.sock").unwrap();
+        let frrmi = FrrMi::new("/tmp/frrmi.sock", "/tmp/frr-agent.sock").unwrap();
 
         /* apply config over frrmi */
-        if let Err(e) = frrmi.apply_config(config.genid(), &rendered) {
+        if let Err(e) = frrmi.apply_config(config.genid(), &rendered).await {
             error!("Failed to apply config: {e:?}");
         } else {
             info!("Successfully applied config");
         }
-
-        /* FIXME: this will only work if|once we have an image with the frr-agent */
     }
 }
