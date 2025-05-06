fb9d9ad7f5946270ad5447c1175c2e22dcf878b9
Fredi Raspall <fredi@githedgehog.com>
2025-05-06 11:58:29 +0200

    feat(mgmt): add ConfigProcessor entity
    The ConfigProcessor owns the config database, an frrmi and opens
    a channel to receive configuration requests. This aims at centra-
    lizing the config management outside of the specific RPC.
    
    Signed-off-by: Fredi Raspall <fredi@githedgehog.com>

--- mgmt/src/processor/proc.rs
+++ mgmt/src/processor/proc.rs
@@ -1,23 +1,27 @@
 // SPDX-License-Identifier: Apache-2.0
 // Copyright Open Network Fabric Authors
 
+#![allow(unused)] // TEMPORARY
+
 use std::io::Error;
 use std::net::SocketAddr;
 use std::sync::Arc;
 use std::thread;
 use tokio::sync::RwLock;
+use tokio::sync::mpsc::Sender;
+use tokio::sync::{mpsc, oneshot};
 use tonic::transport::Server;
 
-use crate::frr::renderer::builder::Render;
 use crate::models::external::gwconfig::ExternalConfig;
 use crate::models::external::{ApiResult, gwconfig::GwConfig};
 use crate::processor::gwconfigdb::GwConfigDatabase;
 use crate::{frr::frrmi::FrrMi, models::external::ApiError};
-use tracing::{debug, error, info};
+use crate::{frr::renderer::builder::Render, models::external::gwconfig::GenId};
+use tracing::{debug, error, info, warn};
 
 use crate::grpc::server::create_config_service;
 
-#[allow(unused)]
+
 /// Build an empty config and apply it
 async fn blank_config_apply(configdb: &mut GwConfigDatabase, frrmi: &FrrMi) {
     let external = ExternalConfig::new();
@@ -31,13 +35,10 @@ pub async fn new_gw_config(
     mut config: GwConfig,
     frrmi: &FrrMi,
 ) -> ApiResult {
-    debug!(
-        "Processing received configuration. Genid:'{}'..",
-        config.genid()
-    );
 
     /* get id of incoming config */
     let genid = config.genid();
+    debug!("Processing config with id:'{genid}'..");
 
     /* reject config if it uses id of existing one */
     if configdb.contains(genid) {
@@ -58,6 +59,82 @@ pub async fn new_gw_config(
     configdb.apply(genid, frrmi).await?;
 
     Ok(())
+}
+
+/// A request type to the [`ConfigProcessor`]
+pub(crate) enum ConfigRequest {
+    ApplyConfig(GwConfig),
+    GetCurrentConfig,
+    GetGeneration,
+}
+
+/// A response from the [`ConfigProcessor`]
+pub(crate) enum ConfigResponse {
+    ApplyConfig(ApiResult),
+    GetCurrentConfig(Option<GwConfig>),
+    GetGeneration(Option<GenId>),
+}
+type ConfigResponseChannel = oneshot::Sender<ConfigResponse>;
+
+/// A type that includes a request to the [`ConfigProcessor`] and a channel to
+/// issue the response back
+struct ConfigChannelRequest {
+    request: ConfigRequest,          /* a request to the mgmt processor */
+    reply_tx: ConfigResponseChannel, /* the one-shot channel to respond */
+}
+
+/// A configuration processor entity. This is the RPC-independent entity responsible for
+/// accepting/rejecting configurations, storing them in the configuration database and
+/// applying them.
+struct ConfigProcessor {
+    config_db: GwConfigDatabase,
+    rx: mpsc::Receiver<ConfigChannelRequest>,
+    frrmi: FrrMi,
+}
+
+impl ConfigProcessor {
+    fn new(frrmi: FrrMi) -> (Self, Sender<ConfigChannelRequest>) {
+        let (tx, rx) = mpsc::channel(1);
+        let processor = Self {
+            config_db: GwConfigDatabase::new(),
+            rx,
+            frrmi,
+        };
+        (processor, tx)
+    }
+    async fn handle_apply_config(&mut self, config: GwConfig) -> ConfigResponse {
+        ConfigResponse::ApplyConfig(new_gw_config(&mut self.config_db, config, &self.frrmi).await)
+    }
+    fn handle_get_generation(&self) -> ConfigResponse {
+        ConfigResponse::GetGeneration(self.config_db.get_current_gen())
+    }
+    fn handle_get_config(&self) -> ConfigResponse {
+        if let Some(current) = self.config_db.get_current_config() {
+            ConfigResponse::GetCurrentConfig(Some(current.clone()))
+        } else {
+            ConfigResponse::GetCurrentConfig(None)
+        }
+    }
+    /// Run the configuration processor
+    async fn run(&mut self) {
+        debug!("Starting config processor...");
+        loop {
+            match self.rx.recv().await {
+                Some(req) => {
+                    let response = match req.request {
+                        ConfigRequest::ApplyConfig(config) => self.handle_apply_config(config).await,
+                        ConfigRequest::GetCurrentConfig => self.handle_get_config(),
+                        ConfigRequest::GetGeneration => self.handle_get_generation(),
+                    };
+                    // check error
+                    req.reply_tx.send(response);
+                }
+                None => {
+                    warn!("Channel to config processor was closed!");
+                }
+            }
+        }
+    }
 }
 
 /// Main logic to apply a [`GwConfig`]. This is called from GwConfig::apply()
