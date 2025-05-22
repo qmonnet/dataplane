724d42a997053ae43460821e01071041211c325f
Fredi Raspall <fredi@githedgehog.com>
2025-05-22 17:37:05 +0200

    feat(dataplane): packet processor - ipforward stage
    Signed-off-by: Fredi Raspall <fredi@githedgehog.com>

---
+++ dataplane/src/packet_processor/ipforward.rs
@@ -0,0 +1,329 @@
+// SPDX-License-Identifier: Apache-2.0
+// Copyright Open Network Fabric Authors
+//
+//! Implements an Ip forwarding stage
+
+use arrayvec::ArrayVec;
+use std::net::IpAddr;
+use tracing::{debug, error, trace, warn};
+
+use net::buffer::PacketBufferMut;
+use net::headers::{TryIpv4Mut, TryIpv6Mut};
+use net::packet::{DoneReason, InterfaceId, Packet};
+use pipeline::NetworkFunction;
+
+use routing::encapsulation::Encapsulation;
+use routing::encapsulation::VxlanEncapsulation;
+use routing::fib::fibtable::FibTableReader;
+use routing::fib::fibtype::FibId;
+use routing::interfaces::interface::IfIndex;
+use routing::route_processor::{EgressObject, FibEntry, PktInstruction};
+use routing::vrf::VrfId;
+
+use net::headers::Headers;
+use net::headers::Net;
+use net::ip::NextHeader;
+use net::ipv4::Ipv4;
+use net::ipv4::UnicastIpv4Addr;
+use net::ipv6::Ipv6;
+use net::ipv6::UnicastIpv6Addr;
+use net::udp::UdpEncap;
+use net::vxlan::Vxlan;
+use net::vxlan::VxlanEncap;
+
+pub struct IpForwarder {
+    name: String,
+    fibtr: FibTableReader,
+}
+
+impl IpForwarder {
+    /// Build a new IP forwarding stage to use the indicated [`FibTableReader`]
+    #[allow(unused)]
+    pub fn new(name: &str, fibtr: FibTableReader) -> Self {
+        Self {
+            name: name.to_owned(),
+            fibtr,
+        }
+    }
+
+    /// Forward a [`Packet`]
+    fn forward_packet<Buf: PacketBufferMut>(&self, packet: &mut Packet<Buf>, vrfid: VrfId) {
+        let nfi = &self.name;
+
+        /* clear any prior VRF annotation in the packet */
+        packet.get_meta_mut().vrf.take();
+
+        /* get destination ip address */
+        let Some(dst) = packet.ip_destination() else {
+            error!("{nfi}: Failed to get destination ip address for packet");
+            packet.done(DoneReason::InternalFailure);
+            return;
+        };
+        debug!("{nfi}: processing packet to {dst} with vrf {vrfid}");
+
+        /* decrement packet TTL -- packet may be done if TTL is exceeded */
+        Self::decrement_ttl(packet, dst);
+        if packet.is_done() {
+            warn!("TTL/Hop-count limit exceeded!");
+            return;
+        }
+
+        /* Get the fib to use: this lookup could be avoided since
+           we know the interface the packet came from and it has to be
+           attached to a certain fib if the vrf metadata value was set.
+           This extra lookup is a side effect of splitting into stages.
+        */
+        if let Some(fibtr) = self.fibtr.enter() {
+            let Some(fibr) = fibtr.get_fib(&FibId::from_vrfid(vrfid)) else {
+                error!("{nfi}: Unable to find fib for vrf {vrfid}");
+                packet.done(DoneReason::InternalFailure);
+                return;
+            };
+            if let Some(fib) = fibr.enter() {
+                let (prefix, fibentry) = fib.lpm_entry_prefix(packet);
+                debug!(
+                    "{nfi}:Packet hit prefix {prefix} in vrf|fib {}, fib entry:\n{}",
+                    fib.get_id().as_u32(),
+                    &fibentry
+                );
+                self.packet_exec_instructions(packet, fibentry);
+            } else {
+                error!("{nfi}: Unable to read fib for vrf {vrfid}");
+                packet.done(DoneReason::InternalFailure);
+            }
+        }
+    }
+
+    /// Execute a local packet instruction
+    fn packet_exec_instruction_local<Buf: PacketBufferMut>(
+        &self,
+        packet: &mut Packet<Buf>,
+        _ifindex: IfIndex,
+    ) {
+        let nfi = &self.name;
+
+        /* packet is destined to gateway. Either we send the packet to the kernel or,
+        if it contains an encapsulated packet (e.g. Vxlan), we send it to the next stage */
+
+        match packet.vxlan_decap() {
+            Some(Ok(vxlan)) => {
+                debug!("{nfi}: DECAPSULATED vxlan packet:\n {packet}");
+                if let Some(fibtable) = self.fibtr.enter() {
+                    let vni = vxlan.vni();
+                    debug!("{nfi}: Packet is for vni {vni}");
+                    if let Some(fib) = fibtable.get_fib(&FibId::from_vni(vni)) {
+                        let next_vrf = fib.get_id().unwrap().as_u32();
+                        debug!("Next fib/vrf is {next_vrf}");
+                        packet.get_meta_mut().vrf = Some(next_vrf);
+                    } else {
+                        error!("{nfi}: Unable to read fib for vni {vni}");
+                        packet.done(DoneReason::InternalFailure);
+                    }
+                }
+            }
+            Some(Err(bad)) => {
+                warn!("oh no, the inner packet is bad: {bad:?}");
+                packet.done(DoneReason::Malformed);
+            }
+            None => {
+                /* send to kernel, among other options */
+                debug!("Packet should be delivered to kernel...");
+                packet.get_meta_mut().oif = Some(packet.get_meta().iif); // FIXME: revisit this
+                packet.done(DoneReason::Delivered);
+            }
+        }
+    }
+
+    /// Build the vxlan headers needed to encapsulate the packet in vxlan
+    fn build_vxlan_headers(vxlan: &VxlanEncapsulation) -> Result<VxlanEncap, ()> {
+        let Some(src_ip) = &vxlan.local else {
+            error!("Failed to build vxlan headers: unknown vxlan local ip");
+            return Err(());
+        };
+
+        // IPv4 or IPv6
+        let net = match (&src_ip, &vxlan.remote) {
+            (IpAddr::V4(src_ip), IpAddr::V4(dst_ip)) => {
+                let src_ip = UnicastIpv4Addr::new(*src_ip).expect("Non-unicast src ip");
+                let mut ip = Ipv4::default();
+                ip.set_source(src_ip).set_destination(*dst_ip).set_ttl(64);
+                unsafe {
+                    ip.set_next_header(NextHeader::UDP);
+                }
+                Net::Ipv4(ip)
+            }
+            (IpAddr::V6(src_ip), IpAddr::V6(dst_ip)) => {
+                let src_ip = UnicastIpv6Addr::new(*src_ip).expect("Non-unicast src ipv6");
+                let mut ip = Ipv6::default();
+                ip.set_source(src_ip)
+                    .set_destination(*dst_ip)
+                    .set_hop_limit(64)
+                    .set_next_header(NextHeader::UDP);
+                Net::Ipv6(ip)
+            }
+            _ => return Err(()),
+        };
+
+        // Encapsulation pseudo header
+        let udp_encap = UdpEncap::Vxlan(Vxlan::new(vxlan.vni));
+
+        // Vxlan encap API headers
+        let headers = Headers {
+            eth: None, /* to be set at egress */
+            vlan: ArrayVec::default(),
+            net: Some(net),
+            net_ext: ArrayVec::default(),
+            transport: None, /* should be UDP, but it is automatically done */
+            udp_encap: Some(udp_encap),
+        };
+        Ok(VxlanEncap::new(headers).unwrap())
+    }
+
+    /// Encapsulate a packet in Vxlan with the provided [`VxlanEncapsulation`] params
+    fn vxlan_encap<Buf: PacketBufferMut>(
+        &self,
+        packet: &mut Packet<Buf>,
+        vxlan: &VxlanEncapsulation,
+    ) {
+        let nfi = &self.name;
+
+        let Some(src_mac) = &vxlan.smac else {
+            packet.done(DoneReason::InternalFailure);
+            return;
+        };
+        let Some(dst_mac) = &vxlan.dmac else {
+            packet.done(DoneReason::InternalFailure);
+            return;
+        };
+
+        // set current packet src mac (inner)
+        if let Err(e) = packet.set_eth_source(*src_mac) {
+            error!("{nfi}: Failed to set src mac: {e} mac: {src_mac}");
+            packet.done(DoneReason::InternalFailure);
+            return;
+        }
+
+        // set current packet dst mac (inner)
+        if let Err(e) = packet.set_eth_destination(*dst_mac) {
+            error!("{nfi}: Failed to set dst mac: {e} mac: {dst_mac}");
+            packet.done(DoneReason::InternalFailure);
+            return;
+        }
+
+        // build vxlan headers for encapsulation
+        let Ok(vxlan_headers) = Self::build_vxlan_headers(vxlan) else {
+            error!("{nfi}: Failed to build VxLAN headers !");
+            packet.done(DoneReason::InternalFailure);
+            return;
+        };
+        match packet.vxlan_encap(&vxlan_headers) {
+            Ok(()) => debug!("{nfi}: ENCAPSULATED packet with VxLAN:\n {packet}"),
+            Err(e) => {
+                error!("{nfi}: Failed to ENCAPSULATE packet with VxLAN: {e}");
+                packet.done(DoneReason::InternalFailure);
+            }
+        }
+    }
+
+    /// Execute an encapsulation instruction on a packet as indicated by [`Encapsulation`]
+    fn packet_exec_instruction_encap<Buf: PacketBufferMut>(
+        &self,
+        packet: &mut Packet<Buf>,
+        encap: &Encapsulation,
+    ) {
+        match encap {
+            Encapsulation::Mpls(_label) => todo!(),
+            Encapsulation::Vxlan(vxlan) => self.vxlan_encap(packet, vxlan),
+        }
+    }
+
+    /// Execute an egress instruction given by the [`EgressObject`] by setting the required metadata
+    /// to send the packet (at an egress stage).
+    fn packet_exec_instruction_egress<Buf: PacketBufferMut>(
+        &self,
+        packet: &mut Packet<Buf>,
+        egress: &EgressObject,
+    ) {
+        let meta = packet.get_meta_mut();
+        if let Some(ifindex) = egress.ifindex() {
+            meta.oif = Some(InterfaceId::new(*ifindex));
+        }
+        if let Some(addr) = egress.address() {
+            meta.nh_addr = Some(*addr);
+        }
+    }
+
+    /// Execute a drop instruction: mark the packet as to drop
+    fn packet_exec_instruction_drop<Buf: PacketBufferMut>(&self, packet: &mut Packet<Buf>) {
+        packet.done(DoneReason::RouteDrop);
+    }
+
+    /// Execute a [`PktInstruction`] on the packet
+    fn packet_exec_instruction<Buf: PacketBufferMut>(
+        &self,
+        packet: &mut Packet<Buf>,
+        instruction: &PktInstruction,
+    ) {
+        match instruction {
+            PktInstruction::Drop => self.packet_exec_instruction_drop(packet),
+            PktInstruction::Local(ifindex) => self.packet_exec_instruction_local(packet, *ifindex),
+            PktInstruction::Encap(encap) => self.packet_exec_instruction_encap(packet, encap),
+            PktInstruction::Egress(egress) => self.packet_exec_instruction_egress(packet, egress),
+            PktInstruction::Nat => {}
+        }
+    }
+
+    /// Execute all of the [`PktInstruction`]s indicated by the given [`FibEntry`] on the packet
+    fn packet_exec_instructions<Buf: PacketBufferMut>(
+        &self,
+        packet: &mut Packet<Buf>,
+        fibentry: &FibEntry,
+    ) {
+        for inst in fibentry.iter() {
+            self.packet_exec_instruction(packet, inst);
+        }
+    }
+
+    /// Decrement the TTL or the hop count for a packet
+    fn decrement_ttl<Buf: PacketBufferMut>(packet: &mut Packet<Buf>, dst_address: IpAddr) {
+        match dst_address {
+            IpAddr::V4(_) => {
+                if let Some(ipv4) = packet.try_ipv4_mut() {
+                    if ipv4.decrement_ttl().is_err() || ipv4.ttl() == 0 {
+                        packet.done(DoneReason::HopLimitExceeded);
+                    }
+                } else {
+                    unreachable!()
+                }
+            }
+            IpAddr::V6(_) => {
+                if let Some(ipv6) = packet.try_ipv6_mut() {
+                    if ipv6.decrement_hop_limit().is_err() || ipv6.hop_limit() == 0 {
+                        packet.done(DoneReason::HopLimitExceeded);
+                    }
+                } else {
+                    unreachable!()
+                }
+            }
+        }
+    }
+}
+
+impl<Buf: PacketBufferMut> NetworkFunction<Buf> for IpForwarder {
+    fn process<'a, Input: Iterator<Item = Packet<Buf>> + 'a>(
+        &'a mut self,
+        input: Input,
+    ) -> impl Iterator<Item = Packet<Buf>> + 'a {
+        trace!("{}'", self.name);
+        input.filter_map(move |mut packet| {
+            if !packet.is_done() {
+                if let Some(vrfid) = packet.get_meta().vrf {
+                    self.forward_packet(&mut packet, vrfid);
+                } else if packet.get_meta().oif.is_none() && !packet.get_meta().is_iplocal {
+                    warn!("{}: missing information to handle packet", self.name);
+                }
+            }
+            packet.enforce()
+        })
+    }
+}
