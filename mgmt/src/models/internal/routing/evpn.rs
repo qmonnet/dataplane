f925a8d1ddf38fc5e18a505b07b93700d0d480d7
Daniel Noland <daniel@githedgehog.com>
2025-05-23 18:39:05 -0600

    fix(mgmt): use validated types for VtepConfig
    1. FRR does not currently support ipv6 in an EVPN underlay.  Thus, restrict the vtep to use ipv4
    2. Multicast is not a legal source ip.  Thus, restrict the vtep to use unicast.
    3. Multicast is not legal in the source mac.  Thus, restrict the vtep mac to unicast.
    
    Signed-off-by: Daniel Noland <daniel@githedgehog.com>

--- mgmt/src/models/internal/routing/evpn.rs
+++ mgmt/src/models/internal/routing/evpn.rs
@@ -3,16 +3,21 @@
 
 //! Dataplane configuration model: EVPN
 
-use net::eth::mac::Mac;
-use std::net::IpAddr;
+use net::eth::mac::{Mac, SourceMac};
+use net::ip::UnicastIpAddr;
 
+/// The configuration of a VTEP (virtual tunnel endpoint) for the Hedgehog EVPN router.
 #[derive(Clone, Debug)]
 pub struct VtepConfig {
-    pub address: IpAddr,
-    pub mac: Mac,
+    /// The source IP address to be used by vxlan packets originating from this router.
+    pub address: UnicastIpAddr,
+    /// The source MAC address to be used by vxlan packets originating from this router.
+    pub mac: SourceMac,
 }
+
 impl VtepConfig {
-    pub fn new(address: IpAddr, mac: Mac) -> Self {
+    /// Creates a new VTEP configuration.
+    pub fn new(address: UnicastIpAddr, mac: SourceMac) -> Self {
         Self { address, mac }
     }
 }
