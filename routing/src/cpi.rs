8ff6734480c2e2fb3c33ebd4c0496ce34418a775
Fredi Raspall <fredi@githedgehog.com>
2025-06-11 11:31:17 +0200

    feat(routing): reorganize CPI and add lock
    Reorganize CPI code defining a CPI object that includes a poller,
    the cpi and cli socks and a control channel. Also, add a lock and
    unlock control messages to allow locking/unlocking to disallow/
    allow updates on the routing database. Explicit unlocking is not
    needed since a lockguard is defined to automatically unlock the
    CPI when the guard goes out of scope in a RAII fashion. This is
    far less error-prone than explicit unlocking.
    
    Signed-off-by: Fredi Raspall <fredi@githedgehog.com>

--- routing/src/cpi.rs
+++ routing/src/cpi.rs
@@ -27,7 +27,7 @@ use std::os::unix::fs::PermissionsExt;
 use std::os::unix::net::UnixDatagram;
 use std::thread::{self, JoinHandle};
 use std::time::Duration;
-use tokio::sync::mpsc::{Sender, channel};
+use tokio::sync::mpsc::{Receiver, Sender, channel};
 use tracing::{debug, error, info};
 
 // capacity of cpi control channel. This should have very little impact on performance.
@@ -96,7 +96,79 @@ fn open_unix_sock(path: &String) -> Result<UnixDatagram, RouterError> {
     Ok(sock)
 }
 
-#[allow(clippy::too_many_lines)]
+pub(crate) const CPSOCK: Token = Token(0);
+pub(crate) const CLISOCK: Token = Token(1);
+
+pub(crate) struct Cpi {
+    pub(crate) run: bool,
+    pub(crate) frozen: bool,
+    pub(crate) cp_sock_path: String,
+    pub(crate) cli_sock_path: String,
+    pub(crate) poller: Poll,
+    pub(crate) clisock: UnixDatagram,
+    pub(crate) cached_sock: RpcCachedSock,
+    pub(crate) ctl_tx: Sender<CpiCtlMsg>,
+    pub(crate) ctl_rx: Receiver<CpiCtlMsg>,
+}
+impl Cpi {
+    fn new(conf: &CpiConf) -> Result<Cpi, RouterError> {
+        /* path to bind to for routing function */
+        let cp_sock_path = conf.cpi_sock_path.as_ref().map_or_else(
+            || DEFAULT_DP_UX_PATH.to_owned(),
+            std::borrow::ToOwned::to_owned,
+        );
+
+        /* path to bind to for cli */
+        let cli_sock_path = conf.cli_sock_path.as_ref().map_or_else(
+            || DEFAULT_DP_UX_PATH_CLI.to_owned(),
+            std::borrow::ToOwned::to_owned,
+        );
+
+        /* create unix sock for routing function and bind it */
+        let cpsock = open_unix_sock(&cp_sock_path)?;
+
+        /* create unix sock for cli and bind it */
+        let clisock = open_unix_sock(&cli_sock_path)?;
+
+        /* internal ctl channel */
+        let (ctl_tx, ctl_rx) = channel::<CpiCtlMsg>(CTL_CHANNEL_CAPACITY);
+
+        /* Routing socket */
+        let cpsock_fd = cpsock.as_raw_fd();
+        let mut ev_cpsock = SourceFd(&cpsock_fd);
+
+        /* Build a cached socket */
+        let cached_sock = RpcCachedSock::from_sock(cpsock);
+
+        /* cli socket */
+        let clisock_fd = clisock.as_raw_fd();
+        let mut ev_clisock = SourceFd(&clisock_fd);
+
+        /* create poller and register cp_sock and cli_sock */
+        let poller = Poll::new().map_err(|_| RouterError::Internal("Poll creation failed"))?;
+        poller
+            .registry()
+            .register(&mut ev_cpsock, CPSOCK, Interest::READABLE)
+            .map_err(|_| RouterError::Internal("Failed to register CPI sock"))?;
+        poller
+            .registry()
+            .register(&mut ev_clisock, CLISOCK, Interest::READABLE)
+            .map_err(|_| RouterError::Internal("Failed to register CLI sock"))?;
+
+        Ok(Cpi {
+            run: true,
+            frozen: false,
+            cp_sock_path,
+            cli_sock_path,
+            poller,
+            clisock,
+            cached_sock,
+            ctl_tx,
+            ctl_rx,
+        })
+    }
+}
+
 #[allow(clippy::missing_errors_doc)]
 pub fn start_cpi(
     conf: &CpiConf,
@@ -104,65 +176,22 @@ pub fn start_cpi(
     iftw: IfTableWriter,
     atabler: AtableReader,
 ) -> Result<CpiHandle, RouterError> {
-    /* path to bind to for routing function */
-    let cp_sock_path = conf.cpi_sock_path.as_ref().map_or_else(
-        || DEFAULT_DP_UX_PATH.to_owned(),
-        std::borrow::ToOwned::to_owned,
-    );
-
-    /* path to bind to for cli */
-    let cli_sock_path = conf.cli_sock_path.as_ref().map_or_else(
-        || DEFAULT_DP_UX_PATH_CLI.to_owned(),
-        std::borrow::ToOwned::to_owned,
-    );
-
-    /* create unix sock for routing function and bind it */
-    let cpsock = open_unix_sock(&cp_sock_path)?;
-
-    /* create unix sock for cli and bind it */
-    let clisock = open_unix_sock(&cli_sock_path)?;
-
-    /* internal ctl channel */
-    let (tx, mut rx) = channel::<CpiCtlMsg>(CTL_CHANNEL_CAPACITY);
-
-    /* Routing socket */
-    const CPSOCK: Token = Token(0);
-    let cpsock_fd = cpsock.as_raw_fd();
-    let mut ev_cpsock = SourceFd(&cpsock_fd);
-
-    /* Build a cached socket */
-    let mut cached_sock = RpcCachedSock::from_sock(cpsock);
-
-    /* cli socket */
-    const CLISOCK: Token = Token(1);
-    let clisock_fd = clisock.as_raw_fd();
-    let mut ev_clisock = SourceFd(&clisock_fd);
-
-    /* create poller and register cp_sock and cli_sock */
-    let mut poller = Poll::new().map_err(|_| RouterError::Internal("Poll creation failed"))?;
-    poller
-        .registry()
-        .register(&mut ev_cpsock, CPSOCK, Interest::READABLE)
-        .map_err(|_| RouterError::Internal("Failed to register CPI sock"))?;
-    poller
-        .registry()
-        .register(&mut ev_clisock, CLISOCK, Interest::READABLE)
-        .map_err(|_| RouterError::Internal("Failed to register CLI sock"))?;
+    let mut cpi = Cpi::new(conf)?;
+    let ctl_tx = cpi.ctl_tx.clone();
 
     /* CPI & CLI loop */
     let cpi_loop = move || {
-        info!("CPI Listening at {}.", cp_sock_path);
-        info!("CLI Listening at {}.", cli_sock_path);
+        info!("CPI Listening at {}.", &cpi.cp_sock_path);
+        info!("CLI Listening at {}.", &cpi.cli_sock_path);
         let mut events = Events::with_capacity(64);
         let mut buf = vec![0; 1024];
-        let mut run = true;
 
         /* create routing database: this is fully owned by the CPI */
         let mut db = RoutingDb::new(fibtw, iftw, atabler);
 
         info!("Entering CPI IO loop....");
-        while run {
-            if let Err(e) = poller.poll(&mut events, Some(Duration::from_secs(1))) {
+        while cpi.run {
+            if let Err(e) = cpi.poller.poll(&mut events, Some(Duration::from_secs(1))) {
                 error!("Poller error!: {e}");
                 continue;
             }
@@ -172,30 +201,30 @@ pub fn start_cpi(
                 match event.token() {
                     CPSOCK => {
                         while event.is_readable() {
-                            if let Ok((len, peer)) = cached_sock.recv_from(buf.as_mut_slice()) {
-                                process_rx_data(&mut cached_sock, &peer, &buf[..len], &mut db);
+                            if let Ok((len, peer)) = cpi.cached_sock.recv_from(buf.as_mut_slice()) {
+                                process_rx_data(&mut cpi.cached_sock, &peer, &buf[..len], &mut db);
                             } else {
                                 break;
                             }
                         }
-                        if event.is_writable() {
-                            cached_sock.flush_out_fast();
-                            if !cached_sock.interests().is_writable() {
-                                if let Err(e) = poller.registry().reregister(
-                                    &mut SourceFd(&cached_sock.get_raw_fd()),
+                        if event.is_writable() && !cpi.frozen {
+                            cpi.cached_sock.flush_out_fast();
+                            if !cpi.cached_sock.interests().is_writable() {
+                                if let Err(e) = cpi.poller.registry().reregister(
+                                    &mut SourceFd(&cpi.cached_sock.get_raw_fd()),
                                     CPSOCK,
-                                    cached_sock.interests(),
+                                    cpi.cached_sock.interests(),
                                 ) {
-                                    error!("Too bad: poller registry failed: {e}");
+                                    error!("Poller reregister failed for CPI: {e} !!!");
                                 }
                             }
                         }
                     }
                     CLISOCK => {
                         while event.is_readable() {
-                            if let Ok((len, peer)) = clisock.recv_from(buf.as_mut_slice()) {
+                            if let Ok((len, peer)) = cpi.clisock.recv_from(buf.as_mut_slice()) {
                                 if let Ok(request) = CliRequest::deserialize(&buf[0..len]) {
-                                    handle_cli_request(&clisock, &peer, request, &db);
+                                    handle_cli_request(&cpi.clisock, &peer, request, &db);
                                 }
                             } else {
                                 break;
@@ -206,8 +235,8 @@ pub fn start_cpi(
                 }
             }
 
-            /* handle control channel messages */
-            handle_ctl_msg(&mut rx, &mut run, &mut db);
+            /* handle control-channel messages */
+            handle_ctl_msg(&mut cpi, &mut db);
         }
     };
     let handle = thread::Builder::new()
@@ -216,7 +245,7 @@ pub fn start_cpi(
         .map_err(|_| RouterError::Internal("Failure spawning thread"))?;
 
     Ok(CpiHandle {
-        ctl: tx,
+        ctl: ctl_tx,
         handle: Some(handle),
     })
 }
--- routing/src/ctl.rs
+++ routing/src/ctl.rs
@@ -3,16 +3,44 @@
 
 //! Control channel for the CPI
 
+use mio::Interest;
+use tokio::sync::mpsc::Sender;
 use tokio::sync::mpsc::error::TryRecvError;
-use tokio::sync::mpsc::{Receiver, Sender};
-use tracing::{error, info, warn};
+use tokio::sync::oneshot;
+use tokio::sync::oneshot::Sender as AsyncSender;
+use tokio::task;
+#[allow(unused)]
+use tracing::{debug, error, info, warn};
 
+use crate::RouterError;
+use crate::cpi::{CPSOCK, Cpi};
 use crate::evpn::Vtep;
 use crate::routingdb::RoutingDb;
+use mio::unix::SourceFd;
+
+type CpiCtlReplyTx = AsyncSender<Result<(), RouterError>>;
+
+#[repr(transparent)]
+pub struct LockGuard(Option<Sender<CpiCtlMsg>>);
+impl Drop for LockGuard {
+    fn drop(&mut self) {
+        let tx = self.0.take();
+        if let Some(tx) = tx {
+            task::spawn(async move {
+                if let Err(e) = tx.send(CpiCtlMsg::GuardedUnlock).await {
+                    error!("Fatal: could not send unlock request!!: {e}");
+                }
+            });
+        }
+    }
+}
 
 pub enum CpiCtlMsg {
     Finish,
     SetVtep(Vtep),
+    Lock(CpiCtlReplyTx),
+    Unlock(CpiCtlReplyTx),
+    GuardedUnlock,
 }
 
 // An object to send control messages to the cpi/router
@@ -21,15 +49,49 @@ impl RouterCtlSender {
     pub(crate) fn new(tx: Sender<CpiCtlMsg>) -> Self {
         Self(tx)
     }
+    pub(crate) fn as_lock_guard(&self) -> LockGuard {
+        LockGuard(Some(self.0.clone()))
+    }
     pub async fn set_vtep(&mut self, vtep: Vtep) {
         if let Err(e) = self.0.send(CpiCtlMsg::SetVtep(vtep)).await {
             error!("Failed to send vtep data: {e} !");
         }
     }
+    #[must_use]
+    pub async fn lock(&mut self) -> Result<LockGuard, RouterError> {
+        debug!("Requesting CPI lock...");
+        let (reply_tx, reply_rx) = oneshot::channel();
+        let msg = CpiCtlMsg::Lock(reply_tx);
+        self.0
+            .send(msg)
+            .await
+            .map_err(|_| RouterError::Internal("Failed to send lock request"))?;
+        let reply = reply_rx
+            .await
+            .map_err(|_| RouterError::Internal("Failed to receive lock reply"))?;
+        reply?;
+        Ok(self.as_lock_guard())
+    }
+    #[allow(unused)]
+    #[must_use]
+    pub async fn unlock(&mut self) -> Result<(), RouterError> {
+        debug!("Requesting CPI lock...");
+        let (reply_tx, reply_rx) = oneshot::channel();
+        let msg = CpiCtlMsg::Unlock(reply_tx);
+        self.0
+            .send(msg)
+            .await
+            .map_err(|_| RouterError::Internal("Failed to send unlock request"))?;
+        let reply = reply_rx
+            .await
+            .map_err(|_| RouterError::Internal("Failed to receive unlock reply"))?;
+        reply?;
+        Ok(())
+    }
 }
 
 /// Handle a control request to set the VTEP ip address and MAC
-fn set_vtep(db: &mut RoutingDb, vtep_data: &Vtep) {
+fn handle_set_vtep(db: &mut RoutingDb, vtep_data: &Vtep) {
     let vtep = &mut db.vtep;
 
     if let Some(ip) = vtep_data.get_ip() {
@@ -53,14 +115,50 @@ fn set_vtep(db: &mut RoutingDb, vtep_data: &Vtep) {
         .for_each(|vrf| vrf.set_vtep(vtep));
 }
 
+/// Handle a lock request for the indicated CPI
+fn handle_lock(cpi: &mut Cpi, lock: bool, reply_to: Option<CpiCtlReplyTx>) {
+    let action = if lock { "lock" } else { "unlock" };
+    let interests = if lock {
+        cpi.frozen = true;
+        Interest::WRITABLE
+    } else {
+        cpi.frozen = false;
+        Interest::WRITABLE | Interest::READABLE
+    };
+    let result = cpi
+        .poller
+        .registry()
+        .reregister(
+            &mut SourceFd(&cpi.cached_sock.get_raw_fd()),
+            CPSOCK,
+            interests,
+        )
+        .map_err(|_| RouterError::Internal("(un)-locking failed"));
+
+    if result.is_ok() {
+        debug!("The CPI is now {action}ed");
+    } else {
+        error!("Failed to {action} CPI");
+    }
+
+    if let Some(reply_to) = reply_to {
+        if let Err(e) = reply_to.send(result) {
+            error!("Fatal: could not reply to lock/unlock request: {e:?}");
+        }
+    }
+}
+
 /// Handle a request from the control channel
-pub(crate) fn handle_ctl_msg(rx: &mut Receiver<CpiCtlMsg>, run: &mut bool, db: &mut RoutingDb) {
-    match rx.try_recv() {
+pub(crate) fn handle_ctl_msg(cpi: &mut Cpi, db: &mut RoutingDb) {
+    match cpi.ctl_rx.try_recv() {
         Ok(CpiCtlMsg::Finish) => {
             info!("Got request to shutdown. Au revoir ...");
-            *run = false;
+            cpi.run = false;
         }
-        Ok(CpiCtlMsg::SetVtep(vtep_data)) => set_vtep(db, &vtep_data),
+        Ok(CpiCtlMsg::SetVtep(vtep_data)) => handle_set_vtep(db, &vtep_data),
+        Ok(CpiCtlMsg::Lock(reply_to)) => handle_lock(cpi, true, Some(reply_to)),
+        Ok(CpiCtlMsg::Unlock(reply_to)) => handle_lock(cpi, false, Some(reply_to)),
+        Ok(CpiCtlMsg::GuardedUnlock) => handle_lock(cpi, false, None),
         Err(TryRecvError::Empty) => {}
         Err(e) => {
             error!("Error receiving from ctl channel {e:?}");
