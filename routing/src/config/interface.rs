0b46bbe799a14f66763848ed7d40db247f31e499
Fredi Raspall <fredi@githedgehog.com>
2025-06-18 19:25:00 +0200

    feat(routing): do not remove interfaces on config
    We can't yet disable auto-learning of interfaces. Therefore, we
    cannot delete interfaces that are not present in the config because
    that would delete the automatically learnt ones.
    
    Signed-off-by: Fredi Raspall <fredi@githedgehog.com>

--- routing/src/config/interface.rs
+++ routing/src/config/interface.rs
@@ -112,7 +112,11 @@ impl ReconfigInterfacePlan {
         iftw: &mut IfTableWriter,
         vrftable: &VrfTable,
     ) -> Result<(), RouterError> {
-        self.enforce_deletions(iftw)?;
+        if false {
+            // disabled because we still auto-learn interfaces. If we'd do this
+            // we'd delete auto-learnt interfaces which is not what we want
+            self.enforce_deletions(iftw)?;
+        }
         self.enforce_changes(iftw, vrftable)?;
         self.enforce_additions(iftw, vrftable)?;
         debug!("Successfully applied Interface configurations");
