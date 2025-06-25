73f85207935c140d3b1982c5d1166a4e0bd2f221
Daniel Noland <daniel@githedgehog.com>
2025-06-25 11:44:51 -0600

    fix: styling of display.rs file
    Various style issues triggering the linter corrected.
    
    The only material change is the renae of a macro to fit the standard rust macro casing convention.
    
    Signed-off-by: Daniel Noland <daniel@githedgehog.com>

--- net/src/interface/display.rs
+++ net/src/interface/display.rs
@@ -8,27 +8,28 @@ use crate::interface::{AdminState, OperationalState};
 use crate::interface::{
     BridgeProperties, Interface, InterfaceProperties, VrfProperties, VtepProperties,
 };
-use std::fmt::Display;
+use std::fmt::{Display, Formatter};
 use std::string::ToString;
 
-macro_rules! KERNEL_INTERFACE_FMT {
+macro_rules! kernel_interface_fmt {
     () => {
         " {:>8} {:>8} {:>16} {:>8} {:>20} {:>8} {:>8} {:>8} {:}"
     };
 }
-fn fmt_kernel_interface_heading(f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+
+fn fmt_kernel_interface_heading(f: &mut Formatter<'_>) -> std::fmt::Result {
     writeln!(
         f,
         "{}",
         format_args!(
-            KERNEL_INTERFACE_FMT!(),
+            kernel_interface_fmt!(),
             "index", "contrl", "name", "mtu", "mac", "Adm", "Oper", "type", "properties"
         )
     )
 }
 
 impl Display for AdminState {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
         match self {
             AdminState::Down => write!(f, "down"),
             AdminState::Up => write!(f, "up"),
@@ -36,7 +37,7 @@ impl Display for AdminState {
     }
 }
 impl Display for OperationalState {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
         match self {
             OperationalState::Down => write!(f, "down"),
             OperationalState::Up => write!(f, "up"),
@@ -47,7 +48,7 @@ impl Display for OperationalState {
 }
 
 impl Display for BridgeProperties {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
         write!(
             f,
             "vlan_filtering: {} vlan-proto: {:?}",
@@ -55,8 +56,9 @@ impl Display for BridgeProperties {
         )
     }
 }
+
 impl Display for VtepProperties {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
         let vni = self
             .vni
             .as_ref()
@@ -72,13 +74,14 @@ impl Display for VtepProperties {
         write!(f, "vni: {vni} ttl: {ttl} local: {local}")
     }
 }
+
 impl Display for VrfProperties {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
         write!(f, "table-id: {}", self.route_table_id)
     }
 }
 impl Display for InterfaceProperties {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
         match self {
             InterfaceProperties::Bridge(bridge) => bridge.fmt(f),
             InterfaceProperties::Vrf(vrf) => vrf.fmt(f),
@@ -98,7 +101,7 @@ fn ifproperty_to_str(properties: &InterfaceProperties) -> &'static str {
 }
 
 impl Display for Interface {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
         let mac = self.mac.map(|mac| mac.to_string()).unwrap_or_default();
         let ctl = self
             .controller
@@ -109,7 +112,7 @@ impl Display for Interface {
             f,
             "{}",
             format_args!(
-                KERNEL_INTERFACE_FMT!(),
+                kernel_interface_fmt!(),
                 self.index.to_string(),
                 ctl,
                 self.name.to_string(),
@@ -123,8 +126,9 @@ impl Display for Interface {
         )
     }
 }
+
 impl Display for MultiIndexInterfaceMap {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
         fmt_kernel_interface_heading(f)?;
         for iface in self.iter_by_index() {
             iface.fmt(f)?;
@@ -142,7 +146,7 @@ where
 }
 
 impl<F: for<'a> Fn(&'a Interface) -> bool> Display for MultiIndexInterfaceMapView<'_, F> {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
         fmt_kernel_interface_heading(f)?;
         let interfaces = self.map.iter().filter(|(_, iface)| (self.filter)(iface));
         for (_, iface) in interfaces {
