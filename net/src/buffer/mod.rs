d28a4378c72621ca22b85fc18ef310e0dce4a753
Daniel Noland <daniel@githedgehog.com>
2025-07-17 15:08:56 -0600

    fix(net)!: Bound PacketBufferMut to TrimFromEnd
    In a future commit I introduce the ability to receive packets from a tap device using tokio.  This implementation requires the ability to trim the packet buffer from the end.
    
    Signed-off-by: Daniel Noland <daniel@githedgehog.com>

--- net/src/buffer/mod.rs
+++ net/src/buffer/mod.rs
@@ -19,11 +19,11 @@ impl<T> PacketBuffer for T where T: AsRef<[u8]> + Headroom + Debug + 'static {}
 
 /// Super trait representing the abstract operations which may be performed on mutable a packet buffer.
 pub trait PacketBufferMut:
-    PacketBuffer + AsMut<[u8]> + Prepend + TrimFromStart + Headroom + Tailroom
+    PacketBuffer + AsMut<[u8]> + Prepend + TrimFromStart + TrimFromEnd + Headroom + Tailroom
 {
 }
 impl<T> PacketBufferMut for T where
-    T: PacketBuffer + AsMut<[u8]> + Prepend + TrimFromStart + Headroom + Tailroom
+    T: PacketBuffer + AsMut<[u8]> + Prepend + TrimFromStart + TrimFromEnd + Headroom + Tailroom
 {
 }
 
