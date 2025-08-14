9e0a3cb06a7244c3266ccccfcd5c4187e23b11f3
Quentin Monnet <qmo@qmon.net>
2025-08-14 17:41:41 +0100

    feat(nat): Extend the NatIp trait used in stateful NAT
    In prevision for upcoming work on the IP and port allocator for stateful
    NAT, extend the NatIp trait with four new methods: try_from_bits(),
    try_from_addr(), try_from_ipv4_addr(), try_from_ipv6_addr(). Implement
    them for both Ipv4Addr and Ipv6Addr.
    
    Signed-off-by: Quentin Monnet <qmo@qmon.net>

--- nat/src/stateful/natip.rs
+++ nat/src/stateful/natip.rs
@@ -1,20 +1,47 @@
 // SPDX-License-Identifier: Apache-2.0
 // Copyright Open Network Fabric Authors
 
+//! NAT IP address trait: a sealed trait to represent either IPv4 or IPv6 in IP-version-generic
+//! code.
+
 use net::headers::Net;
+use std::fmt::Debug;
 use std::hash::Hash;
 use std::net::{IpAddr, Ipv4Addr, Ipv6Addr};
 
+// Keeping this module private provides a "sealed" trait: only types defined in this crate can
+// implement it, external types cannot implement `Sealed`.
 mod private {
     pub trait Sealed {}
 }
-pub trait NatIp: private::Sealed + Clone + Eq + Hash {
+
+/// `NatIp` is a sealed trait to represent either IPv4 or IPv6.
+pub trait NatIp: private::Sealed + Debug + Clone + Copy + Eq + Ord + Hash {
+    // Convert to `IpAddr` object
     fn to_ip_addr(&self) -> IpAddr;
+
+    // Extract the source IP address from a Net object as a `NatIp`
     fn from_src_addr(net: &Net) -> Option<Self>;
+
+    // Extract the destination IP address from a Net object as a `NatIp`
     fn from_dst_addr(net: &Net) -> Option<Self>;
+
+    // Convert from a 128-bit integer to a `NatIp`, if possible
+    fn try_from_bits(bits: u128) -> Result<Self, ()>;
+
+    // Convert from an `IpAddr` object to a `NatIp`, if possible
+    fn try_from_addr(addr: IpAddr) -> Result<Self, ()>;
+
+    // Convert from an `Ipv4Addr` object to a `NatIp`, if possible
+    fn try_from_ipv4_addr(addr: Ipv4Addr) -> Result<Self, ()>;
+
+    // Convert from an `Ipv6Addr` object to a `NatIp`, if possible
+    fn try_from_ipv6_addr(addr: Ipv6Addr) -> Result<Self, ()>;
 }
+
 impl private::Sealed for Ipv4Addr {}
 impl private::Sealed for Ipv6Addr {}
+
 impl NatIp for Ipv4Addr {
     fn to_ip_addr(&self) -> IpAddr {
         IpAddr::V4(*self)
@@ -33,7 +60,24 @@ impl NatIp for Ipv4Addr {
             None
         }
     }
+    fn try_from_bits(bits: u128) -> Result<Self, ()> {
+        Ok(Self::from(u32::try_from(bits).map_err(|_| ())?))
+    }
+    fn try_from_addr(addr: IpAddr) -> Result<Self, ()> {
+        if let IpAddr::V4(addr) = addr {
+            Ok(addr)
+        } else {
+            Err(())
+        }
+    }
+    fn try_from_ipv4_addr(addr: Ipv4Addr) -> Result<Self, ()> {
+        Ok(addr)
+    }
+    fn try_from_ipv6_addr(_addr: Ipv6Addr) -> Result<Self, ()> {
+        Err(())
+    }
 }
+
 impl NatIp for Ipv6Addr {
     fn to_ip_addr(&self) -> IpAddr {
         IpAddr::V6(*self)
@@ -51,5 +95,21 @@ impl NatIp for Ipv6Addr {
         } else {
             None
         }
+    }
+    fn try_from_bits(bits: u128) -> Result<Self, ()> {
+        Ok(Self::from(bits))
+    }
+    fn try_from_addr(addr: IpAddr) -> Result<Self, ()> {
+        if let IpAddr::V6(addr) = addr {
+            Ok(addr)
+        } else {
+            Err(())
+        }
+    }
+    fn try_from_ipv4_addr(_addr: Ipv4Addr) -> Result<Self, ()> {
+        Err(())
+    }
+    fn try_from_ipv6_addr(addr: Ipv6Addr) -> Result<Self, ()> {
+        Ok(addr)
     }
 }
