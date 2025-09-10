96e7fbf5d054830fcce52a859dcf0557ae707831
Manish Vachharajani <manish@githedgehog.com>
2025-09-10 11:23:09 -0600

    test(net): Add ability to build ipv4 test packets with transport
    This commit adds a method, build_test_ipv4_packet_with_transport
    that can create a test packet with a given transport (UDP, TCP)
    for now.  It also consolidates the implementation of ipv4
    test packets around this function.
    
    Signed-off-by: Manish Vachharajani <manish@githedgehog.com>

--- net/src/packet/test_utils.rs
+++ net/src/packet/test_utils.rs
@@ -23,6 +23,7 @@ use crate::ipv6::Ipv6;
 use crate::ipv6::addr::UnicastIpv6Addr;
 use crate::packet::{InvalidPacket, Packet};
 use crate::parse::DeParse;
+use crate::tcp::Tcp;
 use crate::udp::Udp;
 use crate::udp::port::UdpPort;
 use std::default::Default;
@@ -30,13 +31,48 @@ use std::net::{Ipv4Addr, Ipv6Addr};
 use std::str::FromStr;
 
 #[must_use]
-/// Builds a test packet with the given TTL value.
+/// Builds a test ipv4 packet with the given TTL value and transport type.
 ///
-/// The packet is an IPv4 packet with a source IP address of 1.2.3.4 and a destination of 5.6.7.8.
+/// The packet is an IPv4 packet with a source IP address of `1.2.3.4` and a destination of `5.6.7.8`.
 /// The Ethernet source and destination MAC addresses are 0x02:00:00:00:00:01 and 0x02:00:00:00:00:02
-/// respectively.
-pub fn build_test_ipv4_packet(ttl: u8) -> Result<Packet<TestBuffer>, InvalidPacket<TestBuffer>> {
+/// respectively.  The source and destination ports are 123 and 456 respectively for TCP and UDP.
+///
+/// Tests can use the utility functions on [`Packet`] to then customize the addresses and ports as
+/// desired.
+///
+/// # Panics
+///
+/// Panics if the transport type is anything other than `Some(NextHeader::TCP)`, `Some(NextHeader::UDP)`, or None
+///
+pub fn build_test_ipv4_packet_with_transport(
+    ttl: u8,
+    transport_type: Option<NextHeader>,
+) -> Result<Packet<TestBuffer>, InvalidPacket<TestBuffer>> {
     let mut headers = HeadersBuilder::default();
+
+    let mut transport = match transport_type {
+        Some(NextHeader::TCP) => {
+            let mut tcp = Tcp::default();
+            tcp.set_source(123.try_into().unwrap());
+            tcp.set_destination(456.try_into().unwrap());
+            tcp.set_syn(true);
+            tcp.set_sequence_number(1);
+            Some(Transport::Tcp(tcp))
+        }
+
+        Some(NextHeader::UDP) => {
+            let mut udp = Udp::default();
+            udp.set_source(123.try_into().unwrap());
+            udp.set_destination(456.try_into().unwrap());
+            Some(Transport::Udp(udp))
+        }
+
+        Some(transport_type) => panic!(
+            "build_test_ipv4_packet_with_transport: Unsupported transport type: {transport_type:?}"
+        ),
+        None => None,
+    };
+
     headers.eth(Some(Eth::new(
         SourceMac::new(Mac([0x2, 0, 0, 0, 0, 1])).unwrap(),
         DestinationMac::new(Mac([0x2, 0, 0, 0, 0, 2])).unwrap(),
@@ -46,12 +82,98 @@ pub fn build_test_ipv4_packet(ttl: u8) -> Result<Packet<TestBuffer>, InvalidPack
     ipv4.set_source(UnicastIpv4Addr::new(Ipv4Addr::new(1, 2, 3, 4)).unwrap());
     ipv4.set_destination(Ipv4Addr::new(5, 6, 7, 8));
     ipv4.set_ttl(ttl);
-    headers.net(Some(Net::Ipv4(ipv4)));
+    if let Some(transport) = transport.as_ref() {
+        ipv4.set_payload_len(transport.size().get()).unwrap();
+        if let Some(transport_type) = transport_type {
+            ipv4.set_next_header(transport_type);
+        } else {
+            unreachable!("build_test_ipv4_packet_with_transport: Transport type is None here");
+        }
+    }
 
+    let net = Net::Ipv4(ipv4);
+
+    if let Some(transport) = transport.as_mut() {
+        transport.update_checksum(&net, []);
+    }
+
+    headers.net(Some(net));
+    headers.transport(transport);
     let headers = headers.build().unwrap();
     let mut buffer: TestBuffer = TestBuffer::new();
     headers.deparse(buffer.as_mut()).unwrap();
     Packet::new(buffer)
+}
+
+#[must_use]
+/// Builds a test packet with the given TTL value.
+///
+/// The packet is an IPv4 packet with a source IP address of 1.2.3.4 and a destination of 5.6.7.8.
+/// The Ethernet source and destination MAC addresses are 0x02:00:00:00:00:01 and 0x02:00:00:00:00:02
+/// respectively.
+pub fn build_test_ipv4_packet(ttl: u8) -> Result<Packet<TestBuffer>, InvalidPacket<TestBuffer>> {
+    build_test_ipv4_packet_with_transport(ttl, None)
+}
+
+#[must_use]
+/// Build an Ipv4 address from a &str
+pub fn addr_v4(a: &str) -> Ipv4Addr {
+    Ipv4Addr::from_str(a).expect("Bad IPv4 address")
+}
+
+#[must_use]
+#[allow(unsafe_code)]
+/// Builds a UDP/IPv4/Eth frame
+pub fn build_test_udp_ipv4_frame(
+    src_mac: Mac,
+    dst_mac: Mac,
+    src_ip: &str,
+    dst_ip: &str,
+    sport: u16,
+    dport: u16,
+) -> Packet<TestBuffer> {
+    use crate::ip::UnicastIpAddr;
+    use std::net::IpAddr;
+
+    let mut packet = build_test_ipv4_packet_with_transport(255, Some(NextHeader::UDP)).unwrap();
+    packet
+        .set_eth_source(src_mac)
+        .expect("Could not set src mac");
+    packet
+        .set_eth_destination(dst_mac)
+        .expect("Could not set dst mac");
+    packet
+        .set_ip_source(src_ip.parse::<UnicastIpAddr>().expect("Bad src ip"))
+        .expect("Could not set src ip");
+    packet
+        .set_ip_destination(dst_ip.parse::<IpAddr>().expect("Bad dst ip"))
+        .expect("Could not set dst ip");
+    packet
+        .set_udp_source_port(UdpPort::new_checked(sport).expect("Bad src port"))
+        .expect("Could not set src port");
+    packet
+        .set_udp_destination_port(UdpPort::new_checked(dport).expect("Bad dst port"))
+        .expect("Could not set dst port");
+
+    packet
+}
+
+#[must_use]
+#[allow(unsafe_code)]
+pub fn build_test_udp_ipv4_packet(
+    src_ip: &str,
+    dst_ip: &str,
+    sport: u16,
+    dport: u16,
+) -> Packet<TestBuffer> {
+    build_test_udp_ipv4_frame(
+        Mac([0x2, 0, 0, 0, 0, 1]),
+        Mac([0x2, 0, 0, 0, 0, 1]),
+        src_ip,
+        dst_ip,
+        sport,
+        dport,
+    )
 }
 
 #[must_use]
@@ -78,65 +200,4 @@ pub fn build_test_ipv6_packet(ttl: u8) -> Result<Packet<TestBuffer>, InvalidPack
     let mut buffer: TestBuffer = TestBuffer::new();
     headers.deparse(buffer.as_mut()).unwrap();
     Packet::new(buffer)
-}
-
-#[must_use]
-/// Build an Ipv4 address from a &str
-pub fn addr_v4(a: &str) -> Ipv4Addr {
-    Ipv4Addr::from_str(a).expect("Bad IPv4 address")
-}
-
-#[must_use]
-#[allow(unsafe_code)]
-/// Builds a UDP/IPv4/Eth frame
-pub fn build_test_udp_ipv4_frame(
-    src_mac: Mac,
-    dst_mac: Mac,
-    src_ip: &str,
-    dst_ip: &str,
-    sport: u16,
-    dport: u16,
-) -> Packet<TestBuffer> {
-    let mut headers = HeadersBuilder::default();
-
-    headers.eth(Some(Eth::new(
-        SourceMac::new(src_mac).unwrap(),
-        DestinationMac::new(dst_mac).unwrap(),
-        EthType::IPV4,
-    )));
-
-    let mut ipv4 = Ipv4::default();
-    ipv4.set_source(UnicastIpv4Addr::new(addr_v4(src_ip)).expect("Bad unicast IPv4"));
-    ipv4.set_destination(addr_v4(dst_ip));
-    ipv4.set_ttl(255);
-    ipv4.set_next_header(NextHeader::UDP);
-    headers.net(Some(Net::Ipv4(ipv4)));
-
-    let mut udp = Udp::empty();
-    udp.set_source(UdpPort::new_checked(sport).expect("Bad src port"));
-    udp.set_destination(UdpPort::new_checked(dport).expect("Bad dst port"));
-    headers.transport(Some(Transport::Udp(udp)));
-
-    let mut buffer: TestBuffer = TestBuffer::new();
-    headers.build().unwrap().deparse(buffer.as_mut()).unwrap();
-
-    Packet::new(buffer).unwrap()
-}
-
-#[must_use]
-#[allow(unsafe_code)]
-pub fn build_test_udp_ipv4_packet(
-    src_ip: &str,
-    dst_ip: &str,
-    sport: u16,
-    dport: u16,
-) -> Packet<TestBuffer> {
-    build_test_udp_ipv4_frame(
-        Mac([0x2, 0, 0, 0, 0, 1]),
-        Mac([0x2, 0, 0, 0, 0, 1]),
-        src_ip,
-        dst_ip,
-        sport,
-        dport,
-    )
 }
