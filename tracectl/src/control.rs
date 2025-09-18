3cda3888390fdd9de777d598f50ca1604071791a
Fredi Raspall <fredi@githedgehog.com>
2025-09-18 23:09:35 +0200

    feat(tracectl): add some tests
    Signed-off-by: Fredi Raspall <fredi@githedgehog.com>

--- tracectl/src/control.rs
+++ tracectl/src/control.rs
@@ -172,3 +172,151 @@ impl TracingControl {
         info!("{db}");
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use crate::control::{TargetCfg, TracingControl, get_trace_ctl};
+    use tracing::Level;
+    use tracing::level_filters::LevelFilter;
+    use tracing::{debug, error, event, info, trace, warn};
+
+    const TARGET_1: &str = "my-target-1";
+    const TARGET_2: &str = "my-target-2";
+    const TARGET_3: &str = "my-target-3";
+
+    fn log_target1() {
+        println!("logging from target {TARGET_1}");
+        error!(target: TARGET_1, "This is an ERROR log");
+        warn!(target: TARGET_1,  "This is a WARN log");
+        info!(target: TARGET_1,  "This is an INFO log");
+        debug!(target: TARGET_1, "This is a DEBUG log");
+        trace!(target: TARGET_1, "This is a TRACE log");
+    }
+    fn log_target2() {
+        println!("logging from target {TARGET_2}");
+        error!(target: TARGET_2, "This is an ERROR log");
+        warn!(target: TARGET_2,  "This is a WARN log");
+        info!(target: TARGET_2,  "This is an INFO log");
+        debug!(target: TARGET_2, "This is a DEBUG log");
+        trace!(target: TARGET_2, "This is a TRACE log");
+    }
+    fn log_target3() {
+        println!("logging from target {TARGET_3}");
+        error!(target: TARGET_3, "This is an ERROR log");
+        warn!(target: TARGET_3,  "This is a WARN log");
+        info!(target: TARGET_3,  "This is an INFO log");
+        debug!(target: TARGET_3, "This is a DEBUG log");
+        trace!(target: TARGET_3, "This is a TRACE log");
+    }
+
+    #[test]
+    fn test_init() {
+        TracingControl::init();
+        let tctl = get_trace_ctl();
+        info!("The current loglevel is {}", tctl.get_default_level());
+    }
+
+    #[test]
+    fn test_target_levels() {
+        println!();
+        let tctl = get_trace_ctl();
+        tctl.register(TARGET_1, LevelFilter::TRACE, &[TARGET_1]);
+        tctl.register(TARGET_2, LevelFilter::DEBUG, &[TARGET_2]);
+        tctl.register(TARGET_3, LevelFilter::INFO, &[TARGET_3]);
+        println!("{}", tctl.db.lock().unwrap());
+
+        log_target1();
+        log_target2();
+        log_target3();
+
+        tctl.set_tag_level(TARGET_1, LevelFilter::OFF);
+        tctl.set_tag_level(TARGET_2, LevelFilter::WARN);
+        tctl.set_tag_level(TARGET_3, LevelFilter::ERROR);
+
+        log_target1();
+        log_target2();
+        log_target3();
+    }
+
+    #[test]
+    fn test_silent() {
+        println!();
+        let tctl = get_trace_ctl();
+        tctl.set_default_level(LevelFilter::OFF);
+
+        log_target1();
+        log_target2();
+        log_target3();
+
+        tctl.register(TARGET_1, LevelFilter::ERROR, &[TARGET_1]);
+        tctl.register(TARGET_2, LevelFilter::ERROR, &[TARGET_2]);
+        tctl.register(TARGET_3, LevelFilter::ERROR, &[TARGET_3]);
+
+        log_target1();
+        log_target2();
+        log_target3();
+    }
+
+    use crate::targets::TRACING_TARGETS;
+    use crate::trace_target;
+
+    fn some_function1() {
+        trace_target!("func1", LevelFilter::ERROR, &["function1"]);
+    }
+    fn some_function2() {
+        // this won't be registered since the module is already registered
+        trace_target!(LevelFilter::OFF, &["function2"]);
+    }
+
+    #[test]
+    fn test_auto_register_macro() {
+        // declare automatically-named target declaration
+        trace_target!(LevelFilter::ERROR, &["macro-auto"]);
+
+        // declare custom targets
+        trace_target!("target-1", LevelFilter::ERROR, &["target-1"]);
+        trace_target!("target-2", LevelFilter::WARN, &["target-2", "macro-custom"]);
+        trace_target!("target-3", LevelFilter::OFF, &["macro-custom"]);
+
+        // Target presence in static shared slice, even for targets declared later
+        // This is linkme collecting them all at build/link time
+        let static_targets: Vec<&str> = TRACING_TARGETS.iter().map(|c| c.target).collect();
+        assert!(static_targets.contains(&module_path!()));
+        assert!(static_targets.contains(&"target-1"));
+        assert!(static_targets.contains(&"target-2"));
+        assert!(static_targets.contains(&"target-3"));
+        assert!(static_targets.contains(&"target-4")); // defined later
+        assert!(static_targets.contains(&"func1")); // defined in a separate function
+
+        // check target presence in database: all should be there even if defined later
+        let tctl = get_trace_ctl();
+        assert!(tctl.db.lock().unwrap().targets.contains_key(module_path!()));
+        assert!(tctl.db.lock().unwrap().targets.contains_key("target-1"));
+        assert!(tctl.db.lock().unwrap().targets.contains_key("target-2"));
+        assert!(tctl.db.lock().unwrap().targets.contains_key("target-3"));
+        assert!(tctl.db.lock().unwrap().targets.contains_key("target-4"));
+        assert!(tctl.db.lock().unwrap().targets.contains_key("func1"));
+
+        // this is declared after the checks
+        trace_target!("target-4", LevelFilter::OFF, &["target-4"]);
+
+        let tags: Vec<&'static str> = tctl.get_tags().collect();
+        println!("{tags:#?}");
+        println!("{}", tctl.db.lock().unwrap());
+    }
+
+    #[test]
+    fn test_targeted_macro() {
+        use crate::tinfo;
+        const TARGET: &str = "MY-TARGET";
+        trace_target!(TARGET, LevelFilter::TRACE, &["targeted-macro"]);
+        let tctl = get_trace_ctl();
+
+        tinfo!(
+            TARGET,
+            "An info log to target {TARGET} with param1={} param2={}",
+            "hello",
+            54
+        );
+    }
+}
